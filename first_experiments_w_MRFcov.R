8a46e97d98418ad3af84ab88ceb5e7cba10ed03c
Shira Salingre <salingre@mail.tau.ac.il>
2019-02-28 21:44:52 +0200

    going to try and create a fake df to experiment with MRFcov and need to go through updated med df that was just uploaded

--- first_experiments_w_MRFcov.R
+++ first_experiments_w_MRFcov.R
@@ -5,25 +5,27 @@ library(MRFcov)
 library("testthat")
 library("tidyverse")
 library("plotly")
-medata <- read.csv("all_uvc_data_Jan2019.csv")
-medata$X <- NULL
-
-medata_fish <- medata %>% 
-  group_by(data.origin, trans, site, species) %>% # to summarise the amount of same species in each transect
-  summarise(sum = n()) %>%  # count individuals from each group (defined by above variables) to create a df with species abundance for each transect
-  ungroup()
-
-sub_medata_fish <- sample_n(tbl = medata_fish, size = 30) %>%  # sample 30 observations randomly
-  mutate(ID = paste(site, trans, sep = ".")) %>% 
-  select(ID, species, sum) %>% 
-  spread(key = species, value = sum)
-sub_medata_fish[is.na(sub_medata_fish)] <- 0 # change NAs to 0s
-sub_fish_pa <- sub_medata_fish
-rownames(sub_fish_pa) <- sub_fish_pa$ID
-sub_fish_pa <- sub_fish_pa[2:ncol(sub_fish_pa)]
-sub_fish_pa[sub_fish_pa > 0] <- 1 # turn to presence-absence
-
+# medata <- read.csv("all_uvc_data_Jan2019.csv")
+# medata$X <- NULL
+# 
+# medata_fish <- medata %>% 
+#   group_by(data.origin, trans, site, species) %>% # to summarise the amount of same species in each transect
+#   summarise(sum = n()) %>%  # count individuals from each group (defined by above variables) to create a df with species abundance for each transect
+#   ungroup()
+# 
+# sub_medata_fish <- sample_n(tbl = medata_fish, size = 30) %>%  # sample 30 observations randomly
+#   mutate(ID = paste(site, trans, sep = ".")) %>% 
+#   select(ID, species, sum) %>% 
+#   spread(key = species, value = sum)
+# sub_medata_fish[is.na(sub_medata_fish)] <- 0 # change NAs to 0s
+# sub_fish_pa <- sub_medata_fish
+# rownames(sub_fish_pa) <- sub_fish_pa$ID
+# sub_fish_pa <- sub_fish_pa[2:ncol(sub_fish_pa)]
+# sub_fish_pa[sub_fish_pa > 0] <- 1 # turn to presence-absence
 # so now sub_fish_pa is a presence absence 
 
+#########################################################################################################################
+# Create a dataframe to experiment on:
 
+#MRFcov(data = sub_fish_pa, family = "binomial")
 
