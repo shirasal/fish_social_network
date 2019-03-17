097fc7bf70b1efa8091dc9666e5d31d7d5343d6c
Shira Salingre <salingre@mail.tau.ac.il>
2019-03-17 16:29:03 +0200

    wee comment for future work

--- MRFcov_EXPERIMENTS_1.R
+++ MRFcov_EXPERIMENTS_1.R
@@ -2,3 +2,5 @@ library(MRFcov)
 
 birds_binomial <- Bird.parasites[, 1:4]
 MRFcov(birds_binomial, family = "binomial")
+
+### next stage will be to understand the output and it's components and then create a matrix from my data that looks like the sample data and run
