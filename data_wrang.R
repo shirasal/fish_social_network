296219b1a912eefce25bb3abcef13603f1f1be15
Shira Salingre <salingre@mail.tau.ac.il>
2019-04-01 18:27:54 +0300

    trying to add countries with reverse geocode

--- data_wrang.R
+++ data_wrang.R
@@ -1,18 +1,24 @@
+# library(maps)
 library(tidyverse)
-library(plotly)
+# library(plotly)
+library(revgeo)
 
-medata <- read.csv("all_uvc_data_07.03.19_2.csv")
-medata$X.1 <- NULL
-medata$X <- NULL
+# medata <- read.csv("all_uvc_data_07.03.19_2.csv")
+# medata_full$X.1 <- NULL
+# medata_full$X <- NULL
 
-locations_all <- medata[, 4:6]
-locations <- locations_all[!duplicated(locations_all),] %>% 
-  na.omit() %>% 
-  arrange()
-write.csv(x = locations, file = "locations.csv")
+locations <- read.csv("locations.csv", blank.lines.skip = TRUE, skipNul = TRUE) # upload unique locations by lat and lon
+View(locations)
 
-p_loc <- locations %>% 
-  ggplot(aes(x = lon, y = lat)) + geom_point()
+# locations$Country <- map.where(database = "world", locations$lon, locations$lat) # add a country by lat and lon using 'maps' package
+# locations <- locations %>%
+#   mutate(point = paste0(lat ,", ", lon))
+# write.csv(locations, "locations.csv")
 
-ggplotly(p_loc)
+x <- locations$lat
+y <- locations$lon
+
+countries <- revgeo(y, x, output = "frame")
+
+locations_full <- full_join(locations, countries)
 
