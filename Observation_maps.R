37d456e2ee7f39d22b43584589b663c8a729d8ba
shirasal <shira.salin@gmail.com>
2019-08-28 16:20:53 +0300

    Species observation maps created with loop

--- Observation_maps.R
+++ Observation_maps.R
@@ -18,31 +18,31 @@ med_seas <- st_read("~/1Masters/GIS/Med/iho.shp")# For personal laptop
 #     geom_point(aes(x = lon, y = lat), colour = "#F7347A", size = 2, alpha = 0.6))
 # ggsave(filename = "ObserveMaps/boops_boops_obs.png", plot = bb_p)
 # 
-# 1st loop: Separate the species into different df's (stored in a list)
-med_mat <- read_csv("med_species_matrix.csv")
-# Create a data frame with lon, lat and species only:
-med_mat_minimal <- med_mat %>%
-  select(lon, lat, 17:ncol(.))
-
-# Create a loop to subset each species obaservation
-spp_list <- NULL # Create an empty list
-for(i in 3:ncol(med_mat_minimal)) { # for each column in this array
-  spp_list[[i-2]] <- med_mat_minimal %>% # create a new list item, relying on this df
-    select(lon, lat, i) %>%  # select only the column of the specified species
-    filter(.[, 3] > 0) # filter for observations (not 0s)
-}
-# there's some kind of a mistake here and the 2 first list elements are duplicates of Anthias anthias, so I will delete them:
-
-spp_list[[119]]
-
-spp_names <- colnames(med_mat_minimal[3:ncol(med_mat_minimal)])
-names(spp_list) <- spp_names
-names(spp_list)
-# Check if I can find specific object in that list:
-spp_list[["Belone.belone"]]
-# Great success!
-
-saveRDS(spp_list, file = "spp_obs.rds")
+# # 1st loop: Separate the species into different df's (stored in a list)
+# med_mat <- read_csv("med_species_matrix.csv")
+# # Create a data frame with lon, lat and species only:
+# med_mat_minimal <- med_mat %>%
+#   select(lon, lat, 17:ncol(.))
+# 
+# # Create a loop to subset each species obaservation
+# spp_list <- NULL # Create an empty list
+# for(i in 3:ncol(med_mat_minimal)) { # for each column in this array
+#   spp_list[[i-2]] <- med_mat_minimal %>% # create a new list item, relying on this df
+#     select(lon, lat, i) %>%  # select only the column of the specified species
+#     filter(.[, 3] > 0) # filter for observations (not 0s)
+# }
+# # there's some kind of a mistake here and the 2 first list elements are duplicates of Anthias anthias, so I will delete them:
+# 
+# spp_list[[119]]
+# 
+# spp_names <- colnames(med_mat_minimal[3:ncol(med_mat_minimal)])
+# names(spp_list) <- spp_names
+# names(spp_list)
+# # Check if I can find specific object in that list:
+# spp_list[["Belone.belone"]]
+# # Great success!
+# 
+# saveRDS(spp_list, file = "spp_obs.rds")
 
 spp_list <- readRDS("spp_obs.rds")
 
@@ -58,7 +58,7 @@ for (j in 1:length(spp_list)) {
          width = 12, height = 8, units = "in", dpi = 300, limitsize = FALSE, device = "png")
 
 }
-j <- 2
 
-spp_list[[1]]
+list.files(path = "ObserveMaps")
+
        
