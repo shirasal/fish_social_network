39f13a694154111ada16b75c679785711af3cdc4
Shira Salingré <salingre@mail.tau.ac.il>
2019-11-19 12:52:32 +0200

    Created networks for all 3 groups

--- scripts/networks.R
+++ scripts/networks.R
@@ -6,26 +6,43 @@ library(MRFcov)
 med_raw <- read_csv("data/med_raw.csv", col_types = cols(depth = col_double()))
 str(med_raw)
 
+# Find species names in the dataset:
+med_raw %>% filter(grepl(pattern = "cretense", x = .$species)) %>% distinct(species)
+
+# Check abundance of species I'm interested in:
+spp <- list("Epinephelus.costae", "Epinephelus.marginatus", "Epinephelus.caninus",
+              "Epinephelus.aeneus", "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.scriba",
+              "Serranus.hepatus", "Diplodus.annularis", "Diplodus.puntazzo", "Diplodus.sargus",
+              "Diplodus.vulgaris", "Diplodus.cervinus")
+
+med_raw %>% 
+  group_by(species) %>% 
+  summarise(n = sum(sp.n)) %>% 
+  filter(species %in% spp)
+
+
+
 ### Network 1: Groupers and combers
 # list the species (easier to work with vectors)
-groupers <- c("Epinephelus.aeneus", "Epinephelus.caninus", "Epinephelus.costae", "Epinephelus.marginatus",
-              "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.hepatus", "Serranus.scriba")
+groupers <- c("Epinephelus.costae", "Epinephelus.marginatus",
+              "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.scriba")
+# I didn't include species that appear only a few times in the dataset
 
 # Create a species matrix of 'groupers' species with rownames = sites, tmean as covariate
 # this is the format that works with MRF function
 Serranidae <- med_raw %>%
   filter(data.origin != "azz_asi") %>% # azz_asi is only presence-absence
-  mutate(loc = make.unique(paste0(site, "_", trans))) %>% 
+  mutate(loc = paste0(site, "_", trans)) %>% 
   group_by(loc, species, tmean) %>%
   summarise(n = sum(sp.n)) %>% 
   spread(species, n, fill = 0) %>% 
   as.data.frame() %>% 
-  `rownames<-`(.$loc) %>%
-  select(groupers[1:8], tmean) %>% 
-  str()
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(groupers[1:5], tmean)
+str(Serranidae)
 
 # Run MRF with tmean as covariate
-serr_mrf <- MRFcov(data = Serranidae, n_nodes = 8, prep_covariates = TRUE, n_covariates = 1,
+serr_mrf <- MRFcov(data = Serranidae, n_nodes = 5, prep_covariates = TRUE, n_covariates = 1,
                    family = "gaussian")
 
 # Look at co occurrence
@@ -38,6 +55,86 @@ deg <- degree(serr_graph, mode = "all")
 plot.igraph(serr_graph, layout = layout.circle(serr_graph),
             edge.width = abs(E(serr_graph)$weight),
             edge.color = ifelse(E(serr_graph)$weight < 0, '#3399CC', '#FF3333'),
+            vertex.size = deg,
+            vertex.label.family = "sans",
+            vertex.label.font	= 3,
+            vertex.label.cex = 1,
+            vertex.label.color = adjustcolor("#333333", 0.85),
+            vertex.color = adjustcolor("#FFFFFF", .5))
+
+
+
+
+
+### Network 2: Diplodus
+
+# Find species names in the dataset:
+med_raw %>% filter(grepl(pattern = "ghobban", x = .$species)) %>% distinct(species)
+
+# list the species (easier to work with vectors)
+diplodus <- c("Diplodus.annularis", "Diplodus.puntazzo", "Diplodus.sargus",
+              "Diplodus.vulgaris", "Diplodus.cervinus")
+
+Diplodus <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # azz_asi is only presence-absence
+  mutate(loc = paste0(site, "_", trans)) %>% 
+  group_by(loc, species, tmean) %>%
+  summarise(n = sum(sp.n)) %>% 
+  spread(species, n, fill = 0) %>% 
+  as.data.frame() %>% 
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(diplodus[1:5], tmean)
+
+dip_mrf <- MRFcov(data = Diplodus, n_nodes = 5, prep_covariates = TRUE, n_covariates = 1,
+                   family = "gaussian")
+
+plotMRF_hm(dip_mrf)
+
+dip_net <- predict_MRFnetworks(data = Diplodus, MRF_mod = dip_mrf, prep_covariates = TRUE)
+dip_graph <- graph.adjacency(dip_mrf$graph, weighted = T, mode = "undirected")
+deg <- degree(dip_graph, mode = "all")
+plot.igraph(dip_graph, layout = layout.circle(dip_graph),
+            edge.width = abs(E(dip_graph)$weight),
+            edge.color = ifelse(E(dip_graph)$weight < 0, '#3399CC', '#FF3333'),
+            vertex.size = deg,
+            vertex.label.family = "sans",
+            vertex.label.font	= 3,
+            vertex.label.cex = 1,
+            vertex.label.color = adjustcolor("#333333", 0.85),
+            vertex.color = adjustcolor("#FFFFFF", .5))
+
+
+
+
+
+
+### Network 3: Herbivores
+
+herb <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa",
+              "Scarus.ghobban", "Sparisoma.cretense")
+  
+
+Herbiv <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # azz_asi is only presence-absence
+  mutate(loc = paste0(site, "_", trans)) %>% 
+  group_by(loc, species, tmean) %>%
+  summarise(n = sum(sp.n)) %>% 
+  spread(species, n, fill = 0) %>% 
+  as.data.frame() %>% 
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(herb[1:5], tmean)
+
+herb_mrf <- MRFcov(data = Herbiv, n_nodes = 5, prep_covariates = TRUE, n_covariates = 1,
+                  family = "gaussian")
+
+plotMRF_hm(herb_mrf)
+
+herb_net <- predict_MRFnetworks(data = Herbiv, MRF_mod = herb_mrf, prep_covariates = TRUE)
+herb_graph <- graph.adjacency(herb_mrf$graph, weighted = T, mode = "undirected")
+deg <- degree(herb_graph, mode = "all")
+plot.igraph(herb_graph, layout = layout.circle(herb_graph),
+            edge.width = abs(E(herb_graph)$weight),
+            edge.color = ifelse(E(herb_graph)$weight < 0, '#3399CC', '#FF3333'),
             vertex.size = deg,
             vertex.label.family = "sans",
             vertex.label.font	= 3,
