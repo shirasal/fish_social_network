f221bd484091d9e1574c9cf81d4c4eb6a5459b4e
Shira Salingré <salingre@mail.tau.ac.il>
2020-01-08 19:01:27 +0200

    Finished Serranidae (temp + MPAs)

--- scripts/predict_nets.R
+++ scripts/predict_nets.R
@@ -5,12 +5,12 @@ med_raw <- read_csv("data/med_raw.csv", col_types = cols(depth = col_double())) 
   mutate(tmean_reg = scale(tmean)) # regularise temperature covariate
 str(med_raw)
 
-(min_temp <- min(med_raw$tmean)) # 16.8
-(med_temp <- median(med_raw$tmean)) # 20
-(max_temp <- max(med_raw$tmean)) # 23
+# (min_temp <- min(med_raw$tmean)) # 16.8
+# (med_temp <- median(med_raw$tmean)) # 20
+# (max_temp <- max(med_raw$tmean)) # 23
 
 
-### Network 1: Groupers and combers
+### Network 1a: Groupers and combers + temperatures
 groupers <- c("Epinephelus.costae", "Epinephelus.marginatus",
               "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.scriba")
 
@@ -26,32 +26,25 @@ Serranidae <- med_raw %>%
   select(groupers[1:5], tmean_reg)
 str(Serranidae)
 
-serr_mrf_mod <- MRFcov(data = Serranidae, n_nodes = 5, n_covariates = 1, family = "gaussian")
-serr_pred <- predict_MRF(data = Serranidae, MRF_mod = serr_mrf_mod)
-head(serr_pred)
-
+serr_mod <- MRFcov(data = Serranidae, n_nodes = 5, n_covariates = 1, family = "gaussian")
 serr_boot <- bootstrap_MRF(data = Serranidae, n_nodes = 5, n_covariates = 1, family = "gaussian", n_cores = 3)
 serr_pred <- predict_MRF(data = Serranidae, MRF_mod = serr_boot)
 head(serr_pred)
 
-serr_net <- predict_MRFnetworks(data = Serranidae, MRF_mod = serr_mrf_mod)
+## Graphs
 
-cv_MRF_diag(data = Serranidae, n_nodes = 5, family = "gaussian")
-cv_MRF_diag_rep(data = Serranidae, n_nodes = 5, family = "gaussian")
+# min(Serranidae$tmean_reg) # -1.62
+# median(Serranidae$tmean_reg) # 0.18
+# max(Serranidae$tmean_reg) # 1.49
 
-########################################################################################################################################
+# Subset temperature data: low
+Serr_temp_low <- Serranidae %>% filter(between(tmean_reg, -1.62, 0))
 
-min(Serranidae$tmean_reg) # -1.62
-median(Serranidae$tmean_reg) # 0.18
-max(Serranidae$tmean_reg) # 1.49
+serr_mod_low <- MRFcov(data = Serr_temp_low, n_nodes = 5, family = "gaussian")
+serr_pred_low <- predict_MRF(data = Serr_temp_low, MRF_mod = serr_boot) %>% invlogit()
+head(serr_pred_low) # probabilities
 
-Serr_temp_low <- Serranidae %>% select(-tmean_reg) %>% mutate(tmean_reg = -1.5)
-str(Serr_temp_low)
-
-serr_predict_low <- predict_MRF(data = Serr_temp_low, MRF_mod = serr_mrf_mod)
-head(serr_predict_low)
-
-serr_graph_low <- graph.adjacency(serr_predict_low, weighted = T, mode = "undirected")
+serr_graph_low <- graph.adjacency(serr_mod_low$graph, weighted = T, mode = "undirected")
 deg <- degree(serr_graph_low, mode = "all")
 plot.igraph(serr_graph_low, layout = layout.circle(serr_graph_low),
             edge.width = abs(E(serr_graph_low)$weight),
@@ -61,27 +54,106 @@ plot.igraph(serr_graph_low, layout = layout.circle(serr_graph_low),
             vertex.label.font	= 3,
             vertex.label.cex = 1,
             vertex.label.color = adjustcolor("#333333", 0.85),
-            vertex.color = adjustcolor("#FFFFFF", .5))
+            vertex.color = adjustcolor("#FFFFFF", .5),
+            main = "Serranidae network in low temperatures")
 
+# Subset temperature data: med
+Serr_temp_med <- Serranidae %>% filter(between(tmean_reg, 0.00001, 1))
 
-Serr_temp_med <- Serranidae %>% select(-tmean_reg) %>% mutate(tmean_reg = 0.18)
-serr_predict_med <- predict_MRF(data = Serr_temp_med, MRF_mod = serr_mrf, prep_covariates = TRUE)
+serr_mod_med <- MRFcov(data = Serr_temp_med, n_nodes = 5, family = "gaussian")
+serr_pred_med <- predict_MRF(data = Serr_temp_med, MRF_mod = serr_boot) %>% invlogit()
+head(serr_pred_med) # probabilities
 
-Serr_temp_hi <- Serranidae %>% select(-tmean_reg) %>% mutate(tmean_reg = 1.4)
-serr_predict_hi <- predict_MRF(data = Serr_temp_hi, MRF_mod = serr_mrf, prep_covariates = TRUE)
-
-# Create a network and a graph
-serr_net <- predict_MRFnetworks(data = Serranidae, MRF_mod = serr_mrf, prep_covariates = TRUE)
-serr_graph <- graph.adjacency(serr_mrf$graph, weighted = T, mode = "undirected")
-deg <- degree(serr_graph, mode = "all")
-plot.igraph(serr_graph, layout = layout.circle(serr_graph),
-            edge.width = abs(E(serr_graph)$weight),
-            edge.color = ifelse(E(serr_graph)$weight < 0, '#3399CC', '#FF3333'),
+serr_graph_med <- graph.adjacency(serr_mod_med$graph, weighted = T, mode = "undirected")
+deg <- degree(serr_graph_med, mode = "all")
+plot.igraph(serr_graph_med, layout = layout.circle(serr_graph_med),
+            edge.width = abs(E(serr_graph_med)$weight),
+            edge.color = ifelse(E(serr_graph_med)$weight < 0, '#3399CC', '#FF3333'),
             vertex.size = deg,
             vertex.label.family = "sans",
             vertex.label.font	= 3,
             vertex.label.cex = 1,
             vertex.label.color = adjustcolor("#333333", 0.85),
-            vertex.color = adjustcolor("#FFFFFF", .5))
+            vertex.color = adjustcolor("#FFFFFF", .5),
+            main = "Serranidae network in median temperatures")
+
+# Subset temperature data: high
+Serr_temp_hi <- Serranidae %>% filter(between(tmean_reg, 1.00001, 1.49))
+
+serr_mod_hi <- MRFcov(data = Serr_temp_hi, n_nodes = 5, family = "gaussian")
+serr_pred_hi <- predict_MRF(data = Serr_temp_hi, MRF_mod = serr_boot) %>% invlogit()
+head(serr_pred_hi) # probabilities
+
+serr_graph_hi <- graph.adjacency(serr_mod_hi$graph, weighted = T, mode = "undirected")
+deg <- degree(serr_graph_hi, mode = "all")
+plot.igraph(serr_graph_hi, layout = layout.circle(serr_graph_hi),
+            edge.width = abs(E(serr_graph_hi)$weight),
+            edge.color = ifelse(E(serr_graph_hi)$weight < 0, '#3399CC', '#FF3333'),
+            vertex.size = deg,
+            vertex.label.family = "sans",
+            vertex.label.font	= 3,
+            vertex.label.cex = 1,
+            vertex.label.color = adjustcolor("#333333", 0.85),
+            vertex.color = adjustcolor("#FFFFFF", .5),
+            main = "Serranidae network in high temperatures")
 
 
+
+
+### Network 1b: Groupers and combers + MPAs
+groupers <- c("Epinephelus.costae", "Epinephelus.marginatus",
+              "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.scriba")
+
+# Create a species matrix of 'groupers'
+Serranidae <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # azz_asi is only presence-absence
+  mutate(loc = paste0(site, "_", trans)) %>% 
+  group_by(loc, species, enforcement) %>%
+  summarise(n = sum(sp.n)) %>% 
+  spread(species, n, fill = 0) %>% 
+  as.data.frame() %>% 
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(groupers[1:5], enforcement)
+str(Serranidae)
+
+## Graphs
+
+# Subset MPA data: enforcement == 3
+Serr_MPA <- Serranidae %>% filter(enforcement == 3)
+
+serr_mod_MPA <- MRFcov(data = Serr_MPA, n_nodes = 5, family = "gaussian")
+serr_pred_MPA <- predict_MRF(data = Serr_MPA, MRF_mod = serr_boot) %>% invlogit()
+head(serr_pred_MPA) # probabilities
+
+serr_graph_MPA <- graph.adjacency(serr_mod_MPA$graph, weighted = T, mode = "undirected")
+deg <- degree(serr_graph_MPA, mode = "all")
+plot.igraph(serr_graph_MPA, layout = layout.circle(serr_graph_MPA),
+            edge.width = abs(E(serr_graph_MPA)$weight),
+            edge.color = ifelse(E(serr_graph_MPA)$weight < 0, '#3399CC', '#FF3333'),
+            vertex.size = deg,
+            vertex.label.family = "sans",
+            vertex.label.font	= 3,
+            vertex.label.cex = 1,
+            vertex.label.color = adjustcolor("#333333", 0.85),
+            vertex.color = adjustcolor("#FFFFFF", .5),
+            main = "Serranidae network inside MPAs")
+
+# Subset MPA data: enforcement < 3
+Serr_noMPA <- Serranidae %>% filter(enforcement != 3)
+
+serr_mod_noMPA <- MRFcov(data = Serr_noMPA, n_nodes = 5, family = "gaussian")
+serr_pred_noMPA <- predict_MRF(data = Serr_noMPA, MRF_mod = serr_boot) %>% invlogit()
+head(serr_pred_noMPA) # probabilities
+
+serr_graph_noMPA <- graph.adjacency(serr_mod_noMPA$graph, weighted = T, mode = "undirected")
+deg <- degree(serr_graph_noMPA, mode = "all")
+plot.igraph(serr_graph_noMPA, layout = layout.circle(serr_graph_noMPA),
+            edge.width = abs(E(serr_graph_noMPA)$weight),
+            edge.color = ifelse(E(serr_graph_noMPA)$weight < 0, '#3399CC', '#FF3333'),
+            vertex.size = deg,
+            vertex.label.family = "sans",
+            vertex.label.font	= 3,
+            vertex.label.cex = 1,
+            vertex.label.color = adjustcolor("#333333", 0.85),
+            vertex.color = adjustcolor("#FFFFFF", .5),
+            main = "Serranidae network outside MPAs")
