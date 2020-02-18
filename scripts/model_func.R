fc68611a590b3776a3f8d84bf1e73bb4450cdb6e
Shira Salingre <shira.salin@gmail.com>
2020-02-18 11:50:18 +0200

    categorised data function updated

--- scripts/model_func.R
+++ scripts/model_func.R
@@ -23,7 +23,7 @@ run_mod <- function(species_mat, n_covs, family){
   return(list(mod = mod, boot = boot, pred = pred))
 }
 
-# Func 3a: Categorise continuous data ---- Only for continuous data!
+# Func 3: Categorise continuous data
 categorise_cov <- function(species_mat, covariate){
   covariate_vector <- species_mat[[covariate]]
   species_mat %>%
@@ -36,20 +36,24 @@ categorise_cov <- function(species_mat, covariate){
                           ordered_result = TRUE))
 }
 
-# Func 3b: nest data according to categories
-
-nested_data <- function(categorised_data){
-  if_else(!"category" %in% colnames(categorised_data),
-          categorised_data %>%
-            mutate(category = as.factor(.[, ncol(.)])) %>%
-            arrange(category) %>%
-            group_by(category) %>%
-            nest(), 
-          categorised_data %>%
-            arrange(category) %>%
-            group_by(category) %>%
-            nest())
+# Func 3b: Categorise data
+nested_data <- function(categorised_data) {
+  if ("category" %in% colnames(categorised_data)){
+    categorised_data %>%
+      arrange(category) %>%
+      group_by(category) %>%
+      nest() %>% 
+      return()
+  } else {
+    categorised_data %>%
+      mutate(category = as.factor(.[, ncol(.)])) %>%
+      arrange(category) %>%
+      group_by(category) %>%
+      nest() %>% 
+      return()
+  } 
 }
+
 
 
 # Func 4: Run MRFcov model with some defaults
