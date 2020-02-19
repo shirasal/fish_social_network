1c4291152e4aee19d6ed4e58c09d6656d6b2956e
Shira Salingré <salingre@mail.tau.ac.il>
2020-02-19 20:41:36 +0200

    all functions work

--- scripts/running_models.R
+++ scripts/running_models.R
@@ -4,11 +4,10 @@
 source("scripts/pckgs_preps.R")
 source("scripts/model_func.R")
 
-
 # Load raw data -----------------------------------------------------------
 
-med_raw <- read_csv("data/med_raw.csv", col_types = cols(depth = col_double())) %>%
-  filter(data.origin != "azz_asi") %>% # azz_asi is only presence-absence so at the moment it's irrelevant
+med_raw <- read_csv("data/med_raw.csv", col_types = cols(depth = "d")) %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
   mutate(loc = paste0(site, "_", trans), tmean_reg = scale(tmean)) # TODO regularise depth covariate
 str(med_raw)
 pryr::object_size(med_raw)
@@ -26,13 +25,16 @@ herbivores <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa",
                 "Scarus.ghobban", "Sparisoma.cretense")
 
 # TODO THINGS TO ADDRESS:
-# 1. Categorical variable (4 levels, not 3)
-# 2. Add title to graph grid (https://stackoverflow.com/questions/14660372/common-main-title-of-a-figure-panel-compiled-with-parmfrow)
+
+# 2. Add title to graph grid
 # 3. What happens if a species doesn't appear in the data in one of the basins (relevant mainly to herb grp)
 
 
+# GROUPERS ----------------------------------------------------------------
 
-# Run functions on temperature data ---------------------------------------
+# EASTERN BASIN -----------------------------------------------------------
+
+# Temperature as covariate ------------------------------------------------
 
 # Func 1
 spp_mat <- create_spp_mat(dataset = med_raw, basin = east,
@@ -41,13 +43,13 @@ str(spp_mat)
 
 # Func 2
 model_temp <- run_mod(species_mat = spp_mat, n_covs = 1, family = "gaussian")
-model_temp$mod
-model_temp$boot
-str(model_temp$pred)
+coef_temp <- model_temp$mod$direct_coefs
+key_coef_temp <- model_temp$boot$mean_key_coefs
+predict_temp <- model_temp$pred
 
 # Func 3
 categories <- categorise_cov(species_mat = spp_mat, covariate = "tmean_reg")
-nested_cats <- nested_data(categorised_data = categories) # FIXME Error: Can't use numeric NA as column index with `[`.
+nested_cats <- nested_data(categorised_data = categories)
 
 # Func 4
 cat_model <- get_model(data = spp_mat, ncov = 1)
@@ -62,9 +64,11 @@ nested_plot <- nested %>% mutate(plot = map(model, get_graph))
 # Func 8
 temperature_graph <- plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
 
+# png("figures/groupers_east_temp.png", height = 450, width = 1200, res = 150)
+# plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
+# dev.off()
 
-
-# Run functions on MPA data -----------------------------------------------
+# MPA as covariate --------------------------------------------------------
 
 # Func 1
 spp_mat <- create_spp_mat(dataset = med_raw, basin = east,
@@ -73,9 +77,9 @@ str(spp_mat)
 
 # Func 2
 model_enf <- run_mod(species_mat = spp_mat, n_covs = 1, family = "gaussian")
-model_enf$mod
-model_enf$boot
-str(model_enf$pred)
+coef_enf <- model_enf$mod$direct_coefs
+key_coef_enf <- model_enf$boot$mean_key_coefs
+predict_enf <- model_enf$pred
 
 # Func 3b (no need for 3a because it's already categorised)
 nested_cats <- nested_data(categorised_data = spp_mat)
@@ -91,5 +95,42 @@ nested <- nested_models(nested_df = nested_cats)
 nested_plot <- nested %>% mutate(plot = map(model, get_graph))
 
 # Func 8
-MPAs_graph <- plot_multi_graphs(nested_df = nested_data,n_graphs = 3)
+MPAs_graph <- plot_multi_graphs(nested_df = nested_plot, n_graphs = 4) 
 
+# png("figures/groupers_east_mpa.png", height = 450, width = 1600, res = 150)
+# plot_multi_graphs(nested_df = nested_plot, n_graphs = 4)
+# dev.off()
+
+# Depth as covariate ------------------------------------------------------
+
+# Func 1
+spp_mat <- create_spp_mat(dataset = med_raw, basin = east,
+                          group = groupers, covariate = "depth") %>% na.omit()
+str(spp_mat)
+
+# Func 2
+model_dep <- run_mod(species_mat = spp_mat, n_covs = 1, family = "gaussian")
+coef_dep <- model_dep$mod$direct_coefs
+key_coef_dep <- model_dep$boot$mean_key_coefs
+predict_dep <- model_dep$pred
+
+# Func 3
+categories <- categorise_cov(species_mat = spp_mat, covariate = "depth")
+nested_cats <- nested_data(categorised_data = categories)
+
+# Func 4
+cat_model <- get_model(data = spp_mat, ncov = 1)
+cat_model
+
+# Func 5
+nested <- nested_models(nested_df = nested_cats)
+
+# Func 6
+nested_plot <- nested %>% mutate(plot = map(model, get_graph))
+
+# Func 8
+depth_graph <- plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
+
+# png("figures/groupers_east_depth.png", height = 450, width = 1200, res = 150)
+# plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
+# dev.off()
