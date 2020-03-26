bbff26b56e0f8032e9d8062d9d45c2bf1c4c2c1f
Shira Salingre <shira.salin@gmail.com>
2020-03-26 11:58:19 +0200

    created coords dataframe for running MRFcov_spatial()

--- scripts/running_models.R
+++ scripts/running_models.R
@@ -6,16 +6,22 @@ source("scripts/model_func.R")
 
 # Load raw data -----------------------------------------------------------
 
-med_raw <- read_csv("data/med_raw.csv", col_types = cols(depth = "d")) %>%
+med_raw <- read_rds("data/medata.Rds") %>%
   filter(data.origin != "azz_asi") %>% # presence-absence
-  mutate(loc = paste0(site, "_", trans), tmean_reg = scale(tmean)) # TODO regularise depth covariate
+  mutate(loc = paste0(site, "_", trans),
+         tmean_reg = scale(tmean),
+         depth_reg = scale(depth))
+# TODO log abundances?
+# TODO merge protection levels - PROTECTED(2-3)/NOT_PROTECTED(0-1)
 str(med_raw)
 pryr::object_size(med_raw)
 
+med_coords <- med_raw %>% select(loc, lat, lon)
 
 # Define variables `basin`; `group` ---------------------------------------
 west <- list("France", "Italy", "Spain")
 east <- list("Croatia", "Greece", "Israel", "Malta", "Turkey")
+all_med <- list(unique(med_raw$country))
 
 groupers <- c("Epinephelus.costae", "Epinephelus.marginatus",
               "Mycteroperca.rubra", "Serranus.cabrilla", "Serranus.scriba")
@@ -23,12 +29,6 @@ diplodus <- c("Diplodus.annularis", "Diplodus.puntazzo", "Diplodus.sargus",
               "Diplodus.vulgaris", "Diplodus.cervinus")
 herbivores <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa",
                 "Scarus.ghobban", "Sparisoma.cretense")
-
-# TODO THINGS TO ADDRESS:
-
-# 2. Add title to graph grid
-# 3. What happens if a species doesn't appear in the data in one of the basins (relevant mainly to herb grp)
-
 
 # GROUPERS ----------------------------------------------------------------
 
@@ -40,6 +40,7 @@ herbivores <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa",
 spp_mat <- create_spp_mat(dataset = med_raw, basin = east,
                           group = groupers, covariate = "tmean_reg")
 str(spp_mat)
+
 
 # Func 2
 model_temp <- run_mod(species_mat = spp_mat, n_covs = 1, family = "gaussian")
@@ -134,3 +135,78 @@ depth_graph <- plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
 # png("figures/groupers_east_depth.png", height = 450, width = 1200, res = 150)
 # plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
 # dev.off()
+
+
+
+# ### Spatial ### ---------------------------------------------------------
+
+
+# GROUPERS ----------------------------------------------------------------
+
+# Whole Med ---------------------------------------------------------------
+
+# Temperature as covariate ------------------------------------------------
+group <- groupers
+covariate <- "tmean_reg"
+
+# Func 1
+# a) Create species matrix:
+spp_mat <- med_raw %>%
+  group_by(lat, lon, loc, species, tmean_reg, enforcement, depth_reg) %>%
+  summarise(n = sum(sp.n)) %>% 
+  filter(species %in% group) %>% 
+  spread(species, n, fill = 0) %>% 
+  as.data.frame() %>% 
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(group, covariate) %>% 
+  as.matrix()
+str(spp_mat)
+nrow(spp_mat)
+
+# b) Create a coordinate dataframe:
+coords <- med_raw %>%
+  group_by(lat, lon, loc, species, tmean_reg, enforcement, depth_reg) %>%
+  summarise(n = sum(sp.n)) %>% 
+  filter(species %in% group) %>% 
+  spread(species, n, fill = 0) %>% 
+  as.data.frame() %>% 
+  `rownames<-`(make.unique(.$loc)) %>%
+  select(lat, lon)
+nrow(coords)
+class(coords)
+  
+
+# Func 2
+species_mat <- spp_mat
+n_covs <- 1
+family <- "gaussian"
+
+# Run model + Create predictions
+mod <- MRFcov_spatial(data = species_mat, n_nodes = 5,
+              n_covariates = n_covs, family = family, coords = coords, bootstrap = TRUE)
+boot <- bootstrap_MRF(data = species_mat, n_nodes = 5,
+                      n_covariates = n_covs, family = family)
+pred <- predict_MRF(data = species_mat, MRF_mod = boot) %>% invlogit()
+model_temp <- list(mod = mod, boot = boot, pred = pred)
+
+# Func 3
+categories <- categorise_cov(species_mat = species_mat, covariate = "tmean_reg")
+nested_cats <- nested_data(categorised_data = categories)
+
+# Func 4
+cat_model <- get_model(data = spp_mat, ncov = 1)
+cat_model
+
+# Func 5
+nested <- nested_models(nested_df = nested_cats)
+
+# Func 6
+nested_plot <- nested %>% mutate(plot = map(model, get_graph))
+
+# Func 8
+temperature_graph <- plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
+
+# png("figures/groupers_east_temp.png", height = 450, width = 1200, res = 150)
+# plot_multi_graphs(nested_df = nested_plot, n_graphs = 3)
+# dev.off()
+
