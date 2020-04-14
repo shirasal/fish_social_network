61af6db8f93d46b8e55c463584a62aba45ce0b87
Shira Salingre <shira.salin@gmail.com>
2020-04-14 10:06:30 +0300

    igraphs edge weight were scaled

--- scripts/model_func.R
+++ scripts/model_func.R
@@ -104,7 +104,7 @@ plotting_func <- function(igraph, category){
   sub_graph <- igraph
   deg <- degree(sub_graph, mode = "all")
   plot <- plot.igraph(sub_graph, layout = layout.circle(sub_graph),
-                      edge.width = abs(E(sub_graph)$weight),
+                      edge.width = scale(abs(E(sub_graph)$weight)),
                       edge.color = ifelse(E(sub_graph)$weight < 0, '#3399CC', '#FF3333'),
                       vertex.size = deg,
                       vertex.label.family = "sans",
