5c3838ce989aaef33bb8af5a77722c4b8896e7b1
Shira Salingre <shira.salin@gmail.com>
2020-04-16 21:16:14 +0300

    function to calculate sum of mean_coefs

--- scripts/quant_func.R
+++ scripts/quant_func.R
@@ -8,13 +8,25 @@ rel_imp <- function(i, cov){
     sum()
 }
 
-# # Calculate the sum of mean_coefficient, see the general direction
-# mean_coef <- function(j){
-#   j %>% 
-#     .$Mean_coef %>% 
-#     sum()
-# }
-# # Condition here is problematic
+# Calculate the sum of mean_coefficient, see the general direction
+mean_coef <- function(j){
+  j %>% 
+    .$Mean_coef %>% 
+    sum()
+}
 
+## Sum separately negative coefficients and positive coefficients
+# coef_dir <- function(j){
+#   j %>% 
+#     mutate(pos = if_else(.$Mean_coef > 0, sum(), 0),
+#            neg = if_else(.$Mean_coef < 0, sum(), 0)) %>% 
+#   print()
+# }
+## Condition here is problematic 
+## Test above on this:
+# model_temp_grps$boot$mean_key_coefs$Epinephelus.costae %>%
+#   as.data.frame() %>% 
+#   ifelse(Mean_coef > 0,
+#           mutate(pos = sum(), mutate(neg = sum())))
 
 
