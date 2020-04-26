6542573eaa0ca2ec1232b465275aec2cc87228a1
Shira Salingre <shira.salin@gmail.com>
2020-04-26 17:27:41 +0300

    updated to unclude quant_func.R

--- scripts/model_func.R
+++ scripts/model_func.R
@@ -133,3 +133,20 @@ plot_multi_graphs <- function(nested_df, n_graphs){
 }
 
 
+# Function to calculate the sum of effect (relative importance) of species in the model
+rel_imp <- function(i, cov){
+  i %>% 
+    filter(str_detect(string = .$Variable,
+                      pattern = cov, negate = TRUE)) %>% 
+    .$Rel_importance %>% 
+    sum()
+}
+
+# Calculate the sum of mean_coefficient, see the general direction
+mean_coef <- function(j){
+  j %>% 
+    .$Mean_coef %>% 
+    sum()
+}
+
+
