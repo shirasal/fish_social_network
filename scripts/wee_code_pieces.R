6233a504476a0ce420a0af9a08d945742a76ed69
Shira Salingre <shira.salin@gmail.com>
2020-05-17 17:09:20 +0300

    added code to correct listed columns

--- scripts/wee_code_pieces.R
+++ scripts/wee_code_pieces.R
@@ -2,3 +2,6 @@
 new_grps_temp_dep %>% as.data.frame() %>% 
   filter(Epinephelus.costae == 0 & Epinephelus.marginatus == 0 & Mycteroperca.rubra == 0 &
            Serranus.cabrilla == 0 & Serranus.scriba == 0) %>% count()
+
+# Fix for listed columns in a dataframe
+as.data.frame(lapply(med_raw,unlist))
