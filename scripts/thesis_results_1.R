713969c3083c3179ce14ea44d367beca29032c9c
Shira Salingre <shira.salin@gmail.com>
2020-07-30 15:59:27 +0300

    added more function and reorganised

--- scripts/thesis_results_1.R
+++ scripts/thesis_results_1.R
@@ -1,7 +1,9 @@
 
-source("scripts/packages.R")
+source("R/packages.R")
 load("data/all_objects.RData")
-source("scripts/functions.R")
+source("R/functions.R")
+source("R/run_models.R")
+library(wesanderson)
 
 med_clean %>% colnames
 med_clean %>% glimpse()
@@ -25,34 +27,20 @@ med_raw %>% dplyr::filter(!complete.cases(sal_mean) & data.origin == "Sala - PEW
 
 
 # Figure 1. Co-occurrence of species with and without covariates ----------
-
+# Compare between co-occurrence with and without covariates:
 # GROUPERS
-grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 6, family = "gaussian")
 grpsHM_cov <- plotMRF_hm(grps_mod, main = "with covariates")
-
-grps_mod_nocov <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 6, family = "gaussian")
 grpsHM_nocov <- plotMRF_hm(grps_mod_nocov, main = "without covariates")
-
 gridExtra::grid.arrange(grpsHM_cov, grpsHM_nocov, nrow = 1, top = "Groupers co-occurrence")
 
-
 # SEABREAM
-dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 6, family = "gaussian")
 dipHM_cov <- plotMRF_hm(dip_mod, main = "with covariates")
-
-dip_mod_nocov <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 6, family = "gaussian")
 dipHM_nocov <- plotMRF_hm(dip_mod_nocov, main = "without covariates")
-
 gridExtra::grid.arrange(dipHM_cov, dipHM_nocov, nrow = 1, top = "Seabream co-occurrence")
 
-
 # HERBIVORES
-herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 6, family = "gaussian")
 herbHM_cov <- plotMRF_hm(herb_mod, main = "with covariates")
-
-herb_mod_nocov <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 6, family = "gaussian")
 herbHM_nocov <- plotMRF_hm(herb_mod_nocov, main = "without covariates")
-
 gridExtra::grid.arrange(herbHM_cov, herbHM_nocov, nrow = 1, top = "Herbivores co-occurrence")
 
 
@@ -95,8 +83,95 @@ all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>% # Creat
   mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
   ggplot() +
   aes(x = covariate, y = rel_imp, fill = taxa)+
-  stat_summary(geom = "bar", fun = mean, position = "dodge")+
+  stat_summary(geom = "bar", fun = mean, position = "dodge") +
   stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge")
 
-# Does the anthro/biotic/env include also interactions? If so - need to separate them.
+all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>% # Create a tibble of all taxa
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  ggplot() +
+  aes(x = covariate, y = rel_imp, fill = taxa)+
+  stat_summary(geom = "bar", fun = mean, position = "dodge") +
+  stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge") +
+  facet_wrap(~taxa, nrow = 3) +
+  theme(legend.position = "none")
 
+# Rel_imp figures for species:
+# Groupers
+grps_rel_imp_bp <- all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "groupers") %>% 
+  ggplot() +
+  aes(x = covariate, y = rel_imp, fill = species) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "Moonrise3"))
+
+# Diplodus
+dip_rel_imp_bp <- all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "seabream") %>% 
+  ggplot() +
+  aes(x = covariate, y = rel_imp, fill = species) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "FantasticFox"))
+
+# Herbivores
+herb_rel_imp_bp <- all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "herbivores") %>% 
+  ggplot() +
+  aes(x = covariate, y = rel_imp, fill = species) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "Rushmore"))
+
+gridExtra::grid.arrange(grps_rel_imp_bp, dip_rel_imp_bp, herb_rel_imp_bp)
+
+# Exchange covaraite with species (species as x and cov as fill)
+all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "groupers") %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp, fill = covariate) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "Zissou1"))
+
+all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "seabream") %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp, fill = covariate) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "Zissou1"))
+
+all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>%
+  rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  filter(taxa == "herbivores") %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp, fill = covariate) +
+  stat_summary(geom = "bar", fun = mean, position = "stack", alpha = 0.7) + 
+  theme_classic() + 
+  scale_fill_manual(values = wes_palette(n = 5, name = "Zissou1"))
+
+# rel_imp*effect_size for a better index of the importance (when summing up)
+
+
+# Fig. 3: Associations per MPA --------------------------------------------
+
+### How to plot this? How to relate between covariate coefficient and the covariate values? For example: what is the value I'm looking for when I want to see what happens in 25 degrees?
+
+############################################################################
+
+grps_dir_assoc <- direction_assoc(taxa = grps_mod)
+dip_dir_assoc <- direction_assoc(taxa = dip_mod)
+herb_dir_assoc <- direction_assoc(taxa = herb_mod)
