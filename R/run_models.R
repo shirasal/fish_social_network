500aa883bce1d092ece90dc5ec439556a5b7dd18
Shira Salingre <shira.salin@gmail.com>
2020-08-02 17:52:34 +0300

    models without covariates corrected

--- R/run_models.R
+++ R/run_models.R
@@ -7,17 +7,17 @@ source("R/functions.R")
 # Groupers ----------------------------------------------------------------
 
 grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 5, family = "gaussian")
-grps_mod_nocov <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 5, family = "gaussian")
+grps_mod_nocov <- MRFcov(data = grps_mat[1:length(groupers)], n_nodes = length(groupers), family = "gaussian")
 
 # Seabream ----------------------------------------------------------------
 
 dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 5, family = "gaussian")
-dip_mod_nocov <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 5, family = "gaussian")
+dip_mod_nocov <- MRFcov(data = dip_mat[1:length(diplodus)], n_nodes = length(diplodus), family = "gaussian")
 
 # Herbivores --------------------------------------------------------------
 
 herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 5, family = "gaussian")
-herb_mod_nocov <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 5, family = "gaussian")
+herb_mod_nocov <- MRFcov(data = herb_mat[1:length(herbivores)], n_nodes = length(herbivores), family = "gaussian")
 
 # Take a look at the output of the models:
 grps_mod
