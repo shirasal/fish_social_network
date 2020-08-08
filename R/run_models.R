e2700c2acc93b5718e5a0bb4db231d6eaf450bcd
Shira Salingre <shira.salin@gmail.com>
2020-08-08 14:52:46 +0300

    country removed to n_covariates = 4; kept gaussian since abundances are scaled

--- R/run_models.R
+++ R/run_models.R
@@ -6,17 +6,17 @@ source("R/functions.R")
 
 # Groupers ----------------------------------------------------------------
 
-grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 5, family = "gaussian")
+grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 4, family = "gaussian")
 grps_mod_nocov <- MRFcov(data = grps_mat[1:length(groupers)], n_nodes = length(groupers), family = "gaussian")
 
 # Seabream ----------------------------------------------------------------
 
-dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 5, family = "gaussian")
+dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 4, family = "gaussian")
 dip_mod_nocov <- MRFcov(data = dip_mat[1:length(diplodus)], n_nodes = length(diplodus), family = "gaussian")
 
 # Herbivores --------------------------------------------------------------
 
-herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 5, family = "gaussian")
+herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 4, family = "gaussian")
 herb_mod_nocov <- MRFcov(data = herb_mat[1:length(herbivores)], n_nodes = length(herbivores), family = "gaussian")
 
 # Take a look at the output of the models:
@@ -43,3 +43,4 @@ dipHM_nocov <- plotMRF_hm(dip_mod_nocov, main = "without covariates")
 herbHM_cov <- plotMRF_hm(herb_mod, main = "with covariates")
 herbHM_nocov <- plotMRF_hm(herb_mod_nocov, main = "without covariates")
 # gridExtra::grid.arrange(herbHM_cov, herbHM_nocov, nrow = 1, top = "Herbivores co-occurrence")
+
