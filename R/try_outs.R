43c9c31ec496a2f1415c3ca4a5ecf9573b27d837
Shira Salingre <shira.salin@gmail.com>
2020-08-08 15:40:54 +0300

    comparing poisson, transformations

--- R/try_outs.R
+++ R/try_outs.R
@@ -1,3 +1,6 @@
+
+# Check covariates --------------------------------------------------------
+
 med_raw %>% glimpse()
 
 med_raw %>% 
@@ -10,14 +13,84 @@ med_clean %>%
   aes(x = lon, y = temp) +
   geom_point(col = "darkblue")
 
-test_grps_pois <- MRFcov(grps_mat, n_nodes = 5, n_covariates = 5, family = "poisson")
-test_grps_gaus <- MRFcov(grps_mat, n_nodes = 5, n_covariates = 5, family = "gaussian")
+# Poisson vs. Gaussian on transformed -------------------------------------
 
-plotMRF_hm(test_grps_gaus, main = "Gaussian")
-plotMRF_hm(test_grps_pois, main = "Poisson")
+# Create matrix of scaled covariates and abundances to run with 'gaussian':
+med_scaled <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+         temp = scale(tmean),
+         depth = scale(depth),
+         prod = scale(pp_mean),
+         sp.n = scale(sp.n)) %>%
+  select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+spp_mat_scaled <- create_spp_mat(dataset = med_scaled, taxa = groupers, covariate = c(as.array(env_vector), as.array(anthro_vector)))
+
+# Run model on standardised data
+gaus <- MRFcov(spp_mat_scaled, n_nodes = 5, n_covariates = 4, family = "gaussian")
+
+# Create matrix of UNSCALED data
+med_data <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE)) %>% 
+  rename(temp = tmean, depth = depth, prod = pp_mean) %>% 
+  select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+spp_mat <- create_spp_mat(dataset = med_data, taxa = groupers, covariate = c(as.array(env_vector), as.array(anthro_vector)))
+
+# Run model with raw data and family 'poisson'
+pois <- MRFcov(spp_mat, n_nodes = 5, n_covariates = 4, family = "poisson")
+
+plotMRF_hm(gaus, main = "Gaussian")
+plotMRF_hm(pois, main = "Poisson")
 
 
-grps_mat_pa <- grps_mat
-grps_mat_pa[,1:5] <- ifelse(grps_mat_pa[,1:5] > 0, 1, 0)
-grps_mod_pa <- MRFcov(data = grps_mat_pa, n_nodes = length(groupers), n_covariates = 5, family = "binomial")
-plotMRF_hm(grps_mod_pa, main = "Binomial")
+# Run model on presence-absence data --------------------------------------
+pa_mat <- med_data %>%
+  mutate(temp = scale(temp),
+         depth = scale(depth),
+         prod = scale(prod)) %>% 
+  create_spp_mat(taxa = groupers, covariate = c(as.array(env_vector), as.array(anthro_vector)))
+pa_mat[,1:5] <- ifelse(pa_mat[,1:5] > 0, 1, 0)
+pa_mod <- MRFcov(data = pa_mat, n_nodes = length(groupers), n_covariates = 4, family = "binomial")
+plotMRF_hm(pa_mod, main = "Binomial")
+
+
+# Data transformations (abundance) ----------------------------------------
+
+med_raw %>% 
+  filter(species %in% groupers) %>% 
+  ggplot() + 
+  aes(x = species, y = sp.n) +
+  geom_violin(fill = "#eccbae") +
+  theme(axis.text.x = element_text(angle = 45)) +
+  ggtitle("Groupers - raw")
+
+# transformation according to 'vignette("Gaussian_Poisson_CRFs")'
+med_raw %>% 
+  filter(species %in% groupers) %>% 
+  mutate(n_trans = log2(sp.n + 0.1)) %>% 
+  ggplot() + 
+  aes(x = species, y = n_trans) +
+  geom_violin(fill = "#eccbae") +
+  theme(axis.text.x = element_text(angle = 45)) +
+  ggtitle("Groupers - nonpararnormal transformed")
+
+# standartisation
+med_raw %>% 
+  mutate(n_trans = scale(sp.n)) %>% 
+  filter(species %in% groupers) %>% 
+  ggplot() + 
+  aes(x = species, y = n_trans) +
+  geom_violin(fill = "#eccbae") +
+  theme(axis.text.x = element_text(angle = 45)) +
+  ggtitle("Groupers - standaradised per whole dataset")
+
+med_raw %>% 
+  filter(species %in% groupers) %>% 
+  mutate(n_trans = scale(sp.n)) %>% 
+  ggplot() + 
+  aes(x = species, y = n_trans) +
+  geom_violin(fill = "#eccbae") +
+  theme(axis.text.x = element_text(angle = 45)) +
+  ggtitle("Groupers - standaradised per all species in taxa")
+
