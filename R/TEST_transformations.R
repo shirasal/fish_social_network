f6901ef48746a003eba45305399de1cbbc1926ef
Shira Salingré <salingre@mail.tau.ac.il>
2020-08-09 16:32:57 +0300

    added gaussian

--- R/TEST_transformations.R
+++ R/TEST_transformations.R
@@ -54,3 +54,37 @@ gridExtra::grid.arrange(p_npn, p_pois, p_poiscov, nrow = 1)
 rel_imp_sum(pois)
 rel_imp_sum(pois_covstd)
 rel_imp_sum(pois_npn)
+
+# Poisson vs. Gaussian on transformed -------------------------------------
+
+# Create matrix of scaled covariates and abundances to run with 'gaussian':
+med_scaled <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
+         temp = scale(tmean),
+         depth = scale(depth),
+         prod = scale(pp_mean),
+         sp.n = scale(sp.n)) %>%
+  select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+spp_mat_scaled <- create_spp_mat(dataset = med_scaled, taxa = groupers, covariate = c(as.array(env_vector), as.array(anthro_vector)))
+
+# Run model on standardised data
+gaus <- MRFcov(spp_mat_scaled, n_nodes = 5, n_covariates = 4, family = "gaussian")
+
+# Create matrix of UNSCALED data
+med_data <- med_raw %>%
+  filter(data.origin != "azz_asi") %>% # presence-absence
+  mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE)) %>% 
+  rename(temp = tmean, depth = depth, prod = pp_mean) %>% 
+  select(site, lon, lat, trans, species, sp.n, mpa, temp, depth, prod)
+spp_mat <- create_spp_mat(dataset = med_data, taxa = groupers, covariate = c(as.array(env_vector), as.array(anthro_vector)))
+
+# Run model with raw data and family 'poisson'
+pois <- MRFcov(spp_mat, n_nodes = 5, n_covariates = 4, family = "poisson")
+
+p_gaus <- plotMRF_hm(gaus, main = "Gaussian with standardised data")
+p_pois <- plotMRF_hm(pois, main = "Poisson")
+
+gridExtra::grid.arrange(p_gaus, p_pois, nrow = 1)
+
+
