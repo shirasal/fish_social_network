a90913bfdcddef3f9d7955db821a217718be88ce
Shira Salingre <shira.salin@gmail.com>
2020-08-19 18:53:39 +0300

    checked autocorrelations

--- R/TEST_data_checks.R
+++ R/TEST_data_checks.R
@@ -6,12 +6,38 @@ med_raw %>% glimpse()
 med_raw %>% 
   ggplot() +
   aes(x = lon, y = tmean) + 
-  geom_point(col = "darkred")
+  geom_point() +
+  ggtitle("Temp raw (unscaled)")
 
 med_clean %>% 
   ggplot() + 
   aes(x = lon, y = temp) +
-  geom_point(col = "darkblue")
+  geom_point() +
+  ggtitle("Temp scaled (standardised")
+
+med_raw %>% 
+  ggplot() + 
+  aes(x = lon, y = sal_mean) +
+  geom_point() +
+  ggtitle("Sal uscaled")
+
+med_raw %>% 
+  ggplot() + 
+  aes(x = tmean, y = sal_mean) +
+  geom_point() +
+  ggtitle("Sal as function of Temp (raw)")
+
+mpa_vars <- med_raw %>% 
+  select(lon, enforcement, total.mpa.ha, age.reserve.yr) %>% 
+  distinct()
+
+env_vars <- med_raw %>% 
+  select(lon, depth, tmean, sal_mean, pp_mean) %>% 
+  distinct()
+
+pairs(mpa_vars)
+GGally::ggpairs(mpa_vars)
+GGally::ggpairs(env_vars)
 
 # Data transformations (abundance) ----------------------------------------
 
