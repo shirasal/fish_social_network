efca05275932830cea1eec26360ab788932a8a9a
Shira Salingre <shira.salin@gmail.com>
2020-08-24 14:01:10 +0300

    relimp for each taxa by species

--- R/rel_imp.R
+++ R/rel_imp.R
@@ -1,9 +1,7 @@
 # source("R/run_models.R")
-source("R/")# source("R/run_models.R")
-# source("R/run_models_spatial.R")
-
 # rm(list = c("grps_mod_nocov", "dip_mod_nocov", "herb_mod_nocov", 
 #             "grpsHM_nocov", "dipHM_nocov", "herbHM_nocov"))
+# source("R/run_models_spatial.R")
 
 # Figure 2. Relative importance per taxa ----------------------------------
 # How much is does a predictor affect the data
@@ -18,7 +16,7 @@ all_relimp <- list(groupers = grps_relimp,
 # Table of mean relative importance of covariates per taxa:
 relimp_table <- sapply(X = all_relimp, FUN = function(x) x[,-1] %>% colMeans())
 
-all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>% # Create a tibble of all taxa
+all_relimp_p <- all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>% # Create a tibble of all taxa
   rename(taxa = taxa, species = species, covariate = name, rel_imp = value) %>%
   mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
   ggplot() +
@@ -27,3 +25,53 @@ all_relimp %>% bind_rows(.id = "taxa") %>% pivot_longer(3:length(.)) %>% # Creat
   stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge") +
   scale_fill_manual(values = wesanderson::wes_palette(n = 3, name = "Darjeeling2")) +
   labs(title = "Relative importance of factors in the model", subtitle = "All three taxa mean")
+
+ggsave("rel_imp_all_taxa.png", all_relimp_p, "png", "results/", dpi = 150)
+
+# Relative importance of covariates for each taxa -------------------------
+
+grps_relimp_p <- grps_relimp %>% pivot_longer(2:length(.)) %>% # Create a tibble of all species
+  rename(species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp)+
+  stat_summary(geom = "bar", fun = mean, position = "dodge",  fill = "#eccbae") +
+  facet_wrap(~covariate, nrow = 3) +
+  labs(title = "Relative importance of factors in the model", subtitle = "Groupers")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), strip.placement = "outside")
+
+# ggsave("rel_imp_groupers.png", grps_relimp_p, "png", "results/",
+#        dpi = 150, height = 8, width = 7, units = "in")
+
+dip_relimp_p <- dip_relimp %>% pivot_longer(2:length(.)) %>% # Create a tibble of all species
+  rename(species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp)+
+  stat_summary(geom = "bar", fun = mean, position = "dodge",  fill = "#d69c4e") +
+  facet_wrap(~covariate, nrow = 3) +
+  labs(title = "Relative importance of factors in the model", subtitle = "Seabream")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), strip.placement = "outside")
+
+# ggsave("rel_imp_diplodus.png", dip_relimp_p, "png", "results/",
+#        dpi = 150, height = 8, width = 7, units = "in")
+
+herb_relimp_p <- herb_relimp %>% pivot_longer(2:length(.)) %>% # Create a tibble of all species
+  rename(species = species, covariate = name, rel_imp = value) %>%
+  mutate(covariate = str_remove(string = covariate, pattern = "_rel_imp")) %>% 
+  ggplot() +
+  aes(x = species, y = rel_imp)+
+  stat_summary(geom = "bar", fun = mean, position = "dodge",  fill = "#046c9a") +
+  facet_wrap(~covariate, nrow = 3) +
+  labs(title = "Relative importance of factors in the model", subtitle = "Herbivores")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), strip.placement = "outside")
+
+# ggsave("rel_imp_herbs.png", herb_relimp_p, "png", "results/",
+#        dpi = 150, height = 8, width = 7, units = "in")
+
+species_relimp_plots <- gridExtra::grid.arrange(grps_relimp_p, dip_relimp_p, herb_relimp_p,
+                                                nrow = 1)
+
+species_relimp_plots
+
+# ggsave("rel_imp_species.png", species_relimp_plots, "png", "results/", dpi = 150, height = 10, width = 10, units = "in")
