5fbf648fa2b2abecb7365b20aaa97d4035115fd9
Shira Salingre <shira.salin@gmail.com>
2020-08-26 08:58:19 +0300

    commented out parts that are non mandatory for tunning the analysis

--- R/run_models_spatial.R
+++ R/run_models_spatial.R
@@ -15,25 +15,25 @@ grps_coords <- med_coords %>%
 nrow(grps_mat) == nrow(grps_coords)
 grps_spat <- MRFcov_spatial(grps_mat, n_nodes = 5, n_covariates = 4, coords = grps_coords,
                             family = "poisson", n_cores = 8)
-grpsHM_pois <- plotMRF_hm(grps_spat, main = "poisson")
-grpsHM_gaus <- plotMRF_hm(grps_mod, main = "gaussian (unscaled)")
-# When scaled we loose a lot of the associations, only association left is positive E.marg-E.cost
-gridExtra::grid.arrange(grpsHM_gaus, grpsHM_pois, nrow = 1, top = "Groupers co-occurrence")
+# grpsHM_pois <- plotMRF_hm(grps_spat, main = "poisson")
+# grpsHM_gaus <- plotMRF_hm(grps_mod, main = "gaussian (unscaled)")
+# # When scaled we loose a lot of the associations, only association left is positive E.marg-E.cost
+# gridExtra::grid.arrange(grpsHM_gaus, grpsHM_pois, nrow = 1, top = "Groupers co-occurrence")
 
 dip_coords <- med_coords %>% 
   filter(rownames(med_coords) %in% rownames(dip_mat))
 nrow(dip_mat) == nrow(dip_coords)
 dip_spat <- MRFcov_spatial(dip_mat, n_nodes = 5, n_covariates = 4, coords = dip_coords,
                            family = "poisson", n_cores = 8)
-dipHM_pois <- plotMRF_hm(dip_spat, main = "poisson")
-dipHM_gaus <- plotMRF_hm(dip_mod, main = "gaussian (unscaled)")
-gridExtra::grid.arrange(dipHM_gaus, dipHM_pois, nrow = 1, top = "Seabream co-occurrence")
+# dipHM_pois <- plotMRF_hm(dip_spat, main = "poisson")
+# dipHM_gaus <- plotMRF_hm(dip_mod, main = "gaussian (unscaled)")
+# gridExtra::grid.arrange(dipHM_gaus, dipHM_pois, nrow = 1, top = "Seabream co-occurrence")
 
 herb_coords <- med_coords %>% 
   filter(rownames(med_coords) %in% rownames(herb_mat))
 nrow(herb_mat) == nrow(herb_coords)
 herb_spat <- MRFcov_spatial(herb_mat, n_nodes = 5, n_covariates = 4, coords = herb_coords,
                             family = "poisson", n_cores = 8)
-herbHM_pois <- plotMRF_hm(herb_spat, main = "poisson")
-herbHM_gaus <- plotMRF_hm(herb_mod, main = "gaussian (unscaled)")
-gridExtra::grid.arrange(herbHM_gaus, herbHM_pois, nrow = 1, top = "Herbivores co-occurrence")
+# herbHM_pois <- plotMRF_hm(herb_spat, main = "poisson")
+# herbHM_gaus <- plotMRF_hm(herb_mod, main = "gaussian (unscaled)")
+# gridExtra::grid.arrange(herbHM_gaus, herbHM_pois, nrow = 1, top = "Herbivores co-occurrence")
