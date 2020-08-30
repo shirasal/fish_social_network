4ee1a69a46aed2ef9c6489232f67ee7a5d717c5d
Shira Salingré <salingre@mail.tau.ac.il>
2020-08-30 15:50:53 +0300

    need to create coords

--- R/biomass_model.R
+++ R/biomass_model.R
@@ -9,12 +9,20 @@ med_biomass <- med_raw %>%
 
 biomass_mat <- med_biomass %>%
   group_by_at(.vars = c(c("lat", "lon", "site", "trans", "species"), env_vector, anthro_vector)) %>% # group for summarise
-  summarise() %>% # sum biomass for each grouped variable <--------------- needs additional args
-  spread(species, n, fill = 0) %>% # convert to species matrix
+  summarise(mean_biomass = mean(biomass)) %>% # mean biomass for each grouped variable 
+  spread(species, mean_biomass, fill = 0) %>% # convert to species matrix
   ungroup() %>% 
   na.omit() %>% # remove NAs; make sure this part it minimised in the raw data
   mutate(loc = paste(site, trans)) %>% # Create a variable of the location, which shpuld be unique
   group_by(loc) %>% 
   column_to_rownames("loc") %>% # create row names by location
-  select(all_of(groupers, diplodus, herbivores), all_of(env_vector, anthro_vector)) # keep the species and covariates columns
+  select(all_of(groupers), all_of(diplodus), all_of(herbivores), all_of(env_vector), all_of(anthro_vector)) %>%  # keep the species and covariates columns
+  ungroup()
+
+# biomass_coords <- biomass_mat %>% rownames_to_column() %>% 
+
+grps_biomat <- biomass_mat %>% 
+  select(all_of(groupers), all_of(env_vector), all_of(anthro_vector))
+
+grps_bioms_mod <- MRFcov_spatial(grps_biomat, prep_covariates = TRUE, n_nodes = 5, n_covariates = 4, family = "gaussian", coords = grps_coords)
 
