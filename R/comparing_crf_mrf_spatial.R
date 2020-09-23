1bbe24a12392acbdcda2e13545f67d8d3d0cf478
Shira Salingre <shira.salin@gmail.com>
2020-09-23 18:40:57 +0300

    removed quantiles as they are not very important for understanding; added boxplots

--- R/comparing_crf_mrf_spatial.R
+++ R/comparing_crf_mrf_spatial.R
@@ -1,9 +1,14 @@
 source("R/run_models_spatial.R")
 
-
 # Groupers ----------------------------------------------------------------
 
-comparison_spatial <- MRFcov::cv_MRF_diag_rep_spatial(data = grps_mat, coords = grps_coords, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
+comparison_spatial <- MRFcov::cv_MRF_diag_rep_spatial(data = grps_mat, 
+                                                      coords = grps_coords, 
+                                                      compare_null = TRUE, 
+                                                      family = "poisson", 
+                                                      n_nodes = 5, 
+                                                      plot = FALSE)
+MRFcov::cv_MRF_diag_rep_spatial(data = grps_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
 
 Spatial.mse <- quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Spatial MRF")], 0.5)
 Nonspatial.mse <- quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Non-spatial MRF")], 0.5)
@@ -20,6 +25,7 @@ quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Spatial MRF")
 quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Non-spatial MRF")], c(0.025, 0.5, 0.975))
 
 comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = grps_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
+MRFcov::cv_MRF_diag_rep(data = grps_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = TRUE)
 
 MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
 CRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], 0.5)
@@ -30,9 +36,6 @@ if (which.min(c(MRF.mse, CRF.mse)) == 1) {
   best.mod <- "CRF"
 }
 best.mod
-
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], c(0.025, 0.5, 0.975))
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], c(0.025, 0.5, 0.975))
 
 # Diplodus ----------------------------------------------------------------
 
@@ -48,10 +51,6 @@ if (which.min(c(Spatial.mse, Nonspatial.mse)) == 1) {
 }
 best.mod
 
-# Explore fit metrics in more detail using 95% quantiles
-quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Spatial MRF")], c(0.025, 0.5, 0.975))
-quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Non-spatial MRF")], c(0.025, 0.5, 0.975))
-
 comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = dip_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
 
 MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
@@ -63,9 +62,6 @@ if (which.min(c(MRF.mse, CRF.mse)) == 1) {
   best.mod <- "CRF"
 }
 best.mod
-
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], c(0.025, 0.5, 0.975))
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], c(0.025, 0.5, 0.975))
 
 
 # Herbivores --------------------------------------------------------------
@@ -82,10 +78,6 @@ if (which.min(c(Spatial.mse, Nonspatial.mse)) == 1) {
 }
 best.mod
 
-# Explore fit metrics in more detail using 95% quantiles
-quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Spatial MRF")], c(0.025, 0.5, 0.975))
-quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Non-spatial MRF")], c(0.025, 0.5, 0.975))
-
 comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = herb_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
 
 MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
@@ -97,9 +89,6 @@ if (which.min(c(MRF.mse, CRF.mse)) == 1) {
   best.mod <- "CRF"
 }
 best.mod
-
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], c(0.025, 0.5, 0.975))
-quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], c(0.025, 0.5, 0.975))
 
 
 # All turn out to be better models with CRF than MRF and spatial rather than nonspatial
