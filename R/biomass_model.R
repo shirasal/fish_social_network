a41586a0eb0efb4fc0fa136a378d8167980a12f9
Shira Salingre <shira.salin@gmail.com>
2020-10-25 11:42:58 +0200

    ran biomass models for all groups

--- R/biomass_model.R
+++ R/biomass_model.R
@@ -1,28 +1,72 @@
+library(tidyverse)
+library(MRFcov)
+load("data/all_objects.RData")
+med_raw <- read_rds("data/medata.Rds") %>% ungroup()
 
 med_biomass <- med_raw %>% 
   mutate(mpa = if_else(enforcement <= 1, FALSE, TRUE),
          temp = scale(tmean),
          depth = scale(depth),
-         sal = scale(sal_mean),
          prod = scale(pp_mean),
          biomass = a*sp.length^b)
 
-biomass_mat <- med_biomass %>%
-  group_by_at(.vars = c(c("lat", "lon", "site", "trans", "species"), env_vector, anthro_vector)) %>% # group for summarise
-  summarise(mean_biomass = mean(biomass)) %>% # mean biomass for each grouped variable 
-  spread(species, mean_biomass, fill = 0) %>% # convert to species matrix
-  ungroup() %>% 
-  na.omit() %>% # remove NAs; make sure this part it minimised in the raw data
-  mutate(loc = paste(site, trans)) %>% # Create a variable of the location, which shpuld be unique
-  group_by(loc) %>% 
-  column_to_rownames("loc") %>% # create row names by location
-  select(all_of(groupers), all_of(diplodus), all_of(herbivores), all_of(env_vector), all_of(anthro_vector)) %>%  # keep the species and covariates columns
-  ungroup()
+create_biomass_mat <- function(dataset, taxa, covariate){
+  cols <- c(c("lat", "lon", "site", "trans", "species"), env_vector, anthro_vector)
+  dataset %>% 
+    group_by_at(.vars = cols) %>% # group for summarise
+    summarise(n = mean(biomass)) %>% # sum sp.n for each grouped variable
+    spread(species, n, fill = 0) %>% # convert to species matrix
+    ungroup() %>% 
+    na.omit() %>% # remove NAs; make sure this part it minimised in the raw data
+    mutate(loc = paste(site, trans)) %>% # Create a variable of the location, which should be unique
+    group_by(loc) %>% 
+    column_to_rownames("loc") %>% # create row names by location
+    select(all_of(taxa), all_of(covariate)) # keep the species and covariates columns
+}
 
-# biomass_coords <- biomass_mat %>% rownames_to_column() %>% 
 
-grps_biomat <- biomass_mat %>% 
-  select(all_of(groupers), all_of(env_vector), all_of(anthro_vector))
+med_bioms_coords <- med_biomass %>% 
+  distinct(site, trans, lat, lon) %>% 
+  mutate(loc = paste(site, trans)) %>% 
+  column_to_rownames("loc") %>% 
+  select(lon, lat)
 
-grps_bioms_mod <- MRFcov_spatial(grps_biomat, prep_covariates = TRUE, n_nodes = 5, n_covariates = 4, family = "gaussian", coords = grps_coords)
+
+grps_biomat <- create_biomass_mat(dataset = med_biomass,
+                           taxa = groupers,
+                           covariate = c("mpa", "temp", "depth", "prod"))
+grps_coords <- med_bioms_coords %>% 
+  filter(rownames(med_bioms_coords) %in% rownames(grps_biomat))
+nrow(grps_biomat) == nrow(grps_coords)
+
+grps_bioms_mod <- MRFcov_spatial(grps_biomat, prep_covariates = TRUE, n_nodes = 5,
+                                 n_covariates = 4, family = "gaussian", coords = grps_coords)
+
+plotMRF_hm(grps_bioms_mod)
+
+## SEABREAM (Diplodus species)
+dip_biomat <- create_biomass_mat(dataset = med_biomass, taxa = diplodus, covariate = c("mpa", "temp", "depth", "prod"))
+dip_coords <- med_bioms_coords %>% 
+  filter(rownames(med_bioms_coords) %in% rownames(dip_biomat))
+nrow(dip_biomat) == nrow(dip_coords)
+
+dip_bioms_mod <- MRFcov_spatial(dip_biomat, prep_covariates = TRUE, n_nodes = 5,
+                                 n_covariates = 4, family = "gaussian", coords = dip_coords)
+
+plotMRF_hm(dip_bioms_mod)
+
+## HERBIVORES
+herb_biomat <- create_biomass_mat(dataset = med_biomass,
+                                  taxa = herbivores,
+                                  covariate = c("mpa", "temp", "depth", "prod"))
+herb_coords <- med_bioms_coords %>% 
+  filter(rownames(med_bioms_coords) %in% rownames(herb_biomat))
+nrow(herb_biomat) == nrow(herb_coords)
+
+herb_bioms_mod <- MRFcov_spatial(herb_biomat, prep_covariates = TRUE, n_nodes = 4,
+                                 n_covariates = 4, family = "gaussian", coords = herb_coords)
+
+plotMRF_hm(herb_bioms_mod)
+
+
 
