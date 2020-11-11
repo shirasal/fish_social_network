52fe0300cbcbe47cee70f3a2bf99b38d3a340d5a
Shira Salingre <shira.salin@gmail.com>
2020-11-11 20:38:16 +0200

    predictions for main species created

--- R/predictions_pairwise.R
+++ R/predictions_pairwise.R
@@ -153,7 +153,7 @@ dpuntazzo_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>%
 
 # Plot the predictions:
 plot_bar_predictions(dpuntazzo_predictions_mpa, diplodus[[2]])
-# ggsave(filename = str_glue("figures/predictions/Dpuntazzo_Dvulgaris_mpa_nonspat.png"), plot = last_plot(), dpi = 300, device = "png")
+# ggsave(filename = "figures/predictions/Dpuntazzo_Dvulgaris_mpa_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
 
 
 # D. vulgaris prediction
@@ -166,7 +166,7 @@ dvulgaris_abs <- dip_mat %>%
   group_by(mpa) %>% 
   sample_n(10)
 
-dpuntazzo_max <- dip_mat %>% 
+dvulgaris_max <- dip_mat %>% 
   mutate(temp = median(temp),
          depth = median(depth),
          prod = median(prod),
@@ -188,6 +188,7 @@ abs_pred <- dvulgaris_mpa_pred_mat %>%
   pivot_longer(all_of(diplodus),
                names_to = "species",
                values_to = "pred_abs")
+
 pres_pred <- dvulgaris_mpa_pred_mat %>% 
   dplyr::slice_tail(prop = 0.5) %>% 
   pivot_longer(all_of(diplodus),
@@ -201,158 +202,301 @@ dvulgaris_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>%
 
 # Plot the predictions:
 plot_bar_predictions(dvulgaris_predictions_mpa, diplodus[[4]])
-# ggsave(filename = str_glue("figures/predictions/Dvulgaris_Dpuntazzo_mpa_nonspat.png"), plot = last_plot(), dpi = 300, device = "png")
+# ggsave(filename = "figures/predictions/Dvulgaris_Dpuntazzo_mpa_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
 
 
 # D. sargus + D. vulgaris + MPA -------------------------------------------
 
-# E. costae prediction
-ecostae_abs <- grps_mat %>% 
+# D. sargus preditions
+dsargus_abs <- dip_mat %>% 
   mutate(temp = median(temp),
          depth = median(depth),
          prod = median(prod),
-         "Epinephelus.marginatus" = 0,
-         across(all_of(groupers[3:5]), .funs = mean)) %>% 
+         "Diplodus.vulgaris" = 0,
+         across(all_of(c(c(diplodus[1], diplodus[2], diplodus[5]))), .funs = mean)) %>% 
   group_by(mpa) %>% 
   sample_n(10)
 
-ecostae_max <- grps_mat %>% 
+dsargus_max <- dip_mat %>% 
   mutate(temp = median(temp),
          depth = median(depth),
          prod = median(prod),
-         "Epinephelus.marginatus" = max(Epinephelus.marginatus),
-         across(all_of(groupers[3:5]), .funs = mean)) %>% 
+         "Diplodus.vulgaris" = max(Diplodus.vulgaris),
+         across(all_of(c(c(diplodus[1], diplodus[2], diplodus[5]))), .funs = mean)) %>% 
   group_by(mpa) %>% 
   sample_n(10)
 
+dsargus_mpa_pred_mat <- bind_rows(dsargus_abs, dsargus_max)
 
-ecostae_mpa_pred_mat <- bind_rows(ecostae_abs, ecostae_max)
-
-
-ecostae_mpa_predict <- predict_MRF(ecostae_mpa_pred_mat, grps_mod) %>% 
-  `colnames<-`(groupers) %>% 
+dsargus_mpa_predict <- predict_MRF(dsargus_mpa_pred_mat, dip_mod) %>% 
+  `colnames<-`(diplodus) %>% 
   as_data_frame() %>% 
-  mutate(mpa = ecostae_mpa_pred_mat$mpa)
+  mutate(mpa = dsargus_mpa_pred_mat$mpa)
 
 # Plot
-abs_pred <- ecostae_mpa_pred_mat %>% 
+abs_pred <- dsargus_mpa_pred_mat %>% 
   dplyr::slice_head(prop = 0.5) %>% 
-  pivot_longer(all_of(groupers),
+  pivot_longer(all_of(diplodus),
                names_to = "species",
                values_to = "pred_abs")
-pres_pred <- ecostae_mpa_pred_mat %>% 
+pres_pred <- dsargus_mpa_pred_mat %>% 
   dplyr::slice_tail(prop = 0.5) %>% 
-  pivot_longer(all_of(groupers),
+  pivot_longer(all_of(diplodus),
                names_to = "species",
                values_to = "pred_pres")
 
-ecostae_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>% 
+dsargus_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>% 
   pivot_longer(cols = pred_abs:pred_pres,
                names_to = "model",
                values_to = "prediction")
 
 # Plot the predictions:
-plot_bar_predictions(ecostae_predictions_mpa, groupers[[1]])
-# ggsave(filename = str_glue("figures/predictions/Ecostae_Emargin_mpa_nonspat.png"), plot = last_plot(), dpi = 300, device = "png")
+plot_bar_predictions(dsargus_predictions_mpa, diplodus[[3]])
+# ggsave(filename = "figures/predictions/Dsargus_Dvulgaris_mpa_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
 
 
-# E. marginatus prediction
-emargin_abs <- grps_mat %>% 
+# D. vulgaris prediction
+dvulgaris_ds_abs <- dip_mat %>% 
   mutate(temp = median(temp),
          depth = median(depth),
          prod = median(prod),
-         "Epinephelus.costae" = 0,
-         across(all_of(groupers[3:5]), .funs = mean)) %>% 
+         "Diplodus.sargus" = 0,
+         across(all_of(c(c(diplodus[1], diplodus[2], diplodus[5]))), .funs = mean)) %>% 
   group_by(mpa) %>% 
   sample_n(10)
 
-emargin_max <- grps_mat %>% 
+dvulgaris_ds_max <- dip_mat %>% 
   mutate(temp = median(temp),
          depth = median(depth),
          prod = median(prod),
-         "Epinephelus.costae" = max(Epinephelus.costae),
-         across(all_of(groupers[3:5]), .funs = mean)) %>% 
+         "Diplodus.sargus" = max(Diplodus.puntazzo),
+         across(all_of(c(c(diplodus[1], diplodus[2], diplodus[5]))), .funs = mean)) %>% 
   group_by(mpa) %>% 
   sample_n(10)
 
-emargin_mpa_pred_mat <- bind_rows(emargin_abs, emargin_max)
+dvulgaris_ds_mpa_pred_mat <- bind_rows(dvulgaris_ds_abs, dvulgaris_max)
 
-
-emargin_mpa_predict <- predict_MRF(emargin_mpa_pred_mat, grps_mod) %>% 
-  `colnames<-`(groupers) %>% 
+dvulgaris_ds_mpa_predict <- predict_MRF(dvulgaris_ds_mpa_pred_mat, dip_mod) %>% 
+  `colnames<-`(diplodus) %>% 
   as_data_frame() %>% 
-  mutate(mpa = emargin_mpa_pred_mat$mpa)
+  mutate(mpa = dvulgaris_ds_mpa_pred_mat$mpa)
 
 # Plot
-abs_pred <- emargin_mpa_pred_mat %>% 
+abs_pred <- dvulgaris_ds_mpa_pred_mat %>% 
   dplyr::slice_head(prop = 0.5) %>% 
-  pivot_longer(all_of(groupers),
+  pivot_longer(all_of(diplodus),
                names_to = "species",
                values_to = "pred_abs")
-pres_pred <- emargin_mpa_pred_mat %>% 
+
+pres_pred <- dvulgaris_ds_mpa_pred_mat %>% 
   dplyr::slice_tail(prop = 0.5) %>% 
-  pivot_longer(all_of(groupers),
+  pivot_longer(all_of(diplodus),
                names_to = "species",
                values_to = "pred_pres")
 
-emargin_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>% 
+dvulgaris_ds_predictions_mpa <- abs_pred %>% left_join(pres_pred) %>% 
   pivot_longer(cols = pred_abs:pred_pres,
                names_to = "model",
                values_to = "prediction")
 
 # Plot the predictions:
-plot_bar_predictions(emargin_predictions_mpa, groupers[[2]])
-# ggsave(filename = str_glue("figures/predictions/Emargin_Ecostae_mpa_nonspat.png"), plot = last_plot(), dpi = 300, device = "png")
+plot_bar_predictions(dvulgaris_ds_predictions_mpa, diplodus[[4]])
+# ggsave(filename = "figures/predictions/Dvulgaris_Dsargus_mpa_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
 
 
 
 # S. rivulatus + S. luridus -----------------------------------------------
 
-herb_temp_abs <- herb_mat %>% 
+# S rivulatus predition
+sriv_temp_abs <- herb_mat %>% 
   mutate(depth = median(depth),
          prod = median(prod),
          mpa = TRUE,
-         across(all_of(herbivores), function(x) 0)) %>% 
+         Siganus.luridus = 0,
+         across(all_of(herbivores[3:4]), .funs = mean)) %>% 
   group_by(temp = round(temp, digits = 1)) %>% 
   sample_n(1)
 
-herb_temp_max <- herb_mat %>% 
+sriv_temp_max <- herb_mat %>% 
   mutate(depth = median(depth),
          prod = median(prod),
          mpa = TRUE,
-         across(all_of(herbivores), .fns = max)) %>% 
+         Siganus.luridus = max(Siganus.luridus),
+         across(all_of(herbivores[3:4]), .fns = mean)) %>% 
   group_by(temp = round(temp, digits = 1)) %>% 
   sample_n(1)
 
-herb_temp_pred_mat <- bind_rows(herb_temp_abs, herb_temp_max)
+sriv_temp_pred_mat <- bind_rows(sriv_temp_abs, sriv_temp_max)
 
 
-herb_temp_predict <- predict_MRF(herb_temp_pred_mat, herb_mod) %>% 
+sriv_temp_predict <- predict_MRF(sriv_temp_pred_mat, herb_mod) %>% 
   `colnames<-`(herbivores) %>% 
   as_data_frame() %>% 
-  mutate(temp = herb_temp_pred_mat$temp)
+  mutate(temp = sriv_temp_pred_mat$temp)
 
 # Plot
-abs_pred <- herb_temp_predict %>% 
+abs_pred <- sriv_temp_predict %>% 
   dplyr::slice_head(prop = 0.5) %>% 
   pivot_longer(all_of(herbivores),
                names_to = "species",
                values_to = "pred_abs")
-pres_pred <- herb_temp_predict %>% 
+pres_pred <- sriv_temp_predict %>% 
   dplyr::slice_tail(prop = 0.5) %>% 
   pivot_longer(all_of(herbivores),
                names_to = "species",
                values_to = "pred_pres")
 
-herb_predictions <- abs_pred %>% left_join(pres_pred) %>% 
+sriv_predictions <- abs_pred %>% left_join(pres_pred) %>% 
   pivot_longer(cols = pred_abs:pred_pres,
                names_to = "model",
                values_to = "prediction")
 
 # Plot the predictions:
-herb_temp_plots <- list()
-for(i in herbivores) { # for each species in this array
-  herb_temp_plots[[i]] <- plot_predictions(herb_predictions, i)
-  # ggsave(filename = str_glue("figures/predictions/{i}_temp_nonspat.png"), plot = last_plot(), dpi = 300, device = "png")
-}
+plot_predictions(sriv_predictions, herbivores[[1]])
+# ggsave(filename = "figures/predictions/Srivulatus_Sluridus_temp_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
+
+# S rivulatus predition
+sriv_temp_abs <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.luridus = 0,
+         across(all_of(herbivores[3:4]), .funs = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+sriv_temp_max <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.luridus = max(Siganus.luridus),
+         across(all_of(herbivores[3:4]), .fns = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+sriv_temp_pred_mat <- bind_rows(sriv_temp_abs, sriv_temp_max)
+
+
+sriv_temp_predict <- predict_MRF(sriv_temp_pred_mat, herb_mod) %>% 
+  `colnames<-`(herbivores) %>% 
+  as_data_frame() %>% 
+  mutate(temp = sriv_temp_pred_mat$temp)
+
+# Plot
+abs_pred <- sriv_temp_predict %>% 
+  dplyr::slice_head(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_abs")
+pres_pred <- sriv_temp_predict %>% 
+  dplyr::slice_tail(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_pres")
+
+sriv_predictions <- abs_pred %>% left_join(pres_pred) %>% 
+  pivot_longer(cols = pred_abs:pred_pres,
+               names_to = "model",
+               values_to = "prediction")
+
+# Plot the predictions:
+plot_predictions(sriv_predictions, herbivores[[1]])
+# ggsave(filename = "figures/predictions/Srivulatus_Sluridus_temp_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
+
+# S rivulatus predition
+sriv_temp_abs <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.luridus = 0,
+         across(all_of(herbivores[3:4]), .funs = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+sriv_temp_max <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.luridus = max(Siganus.luridus),
+         across(all_of(herbivores[3:4]), .fns = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+sriv_temp_pred_mat <- bind_rows(sriv_temp_abs, sriv_temp_max)
+
+
+sriv_temp_predict <- predict_MRF(sriv_temp_pred_mat, herb_mod) %>% 
+  `colnames<-`(herbivores) %>% 
+  as_data_frame() %>% 
+  mutate(temp = sriv_temp_pred_mat$temp)
+
+# Plot
+abs_pred <- sriv_temp_predict %>% 
+  dplyr::slice_head(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_abs")
+pres_pred <- sriv_temp_predict %>% 
+  dplyr::slice_tail(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_pres")
+
+sriv_predictions <- abs_pred %>% left_join(pres_pred) %>% 
+  pivot_longer(cols = pred_abs:pred_pres,
+               names_to = "model",
+               values_to = "prediction")
+
+# Plot the predictions:
+plot_predictions(sriv_predictions, herbivores[[1]])
+# ggsave(filename = "figures/predictions/Srivulatus_Sluridus_temp_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
+
+
+# S luridus prediction
+slurid_temp_abs <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.rivulatus = 0,
+         across(all_of(herbivores[3:4]), .funs = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+slurid_temp_max <- herb_mat %>% 
+  mutate(depth = median(depth),
+         prod = median(prod),
+         mpa = TRUE,
+         Siganus.rivulatus = max(Siganus.rivulatus),
+         across(all_of(herbivores[3:4]), .fns = mean)) %>% 
+  group_by(temp = round(temp, digits = 1)) %>% 
+  sample_n(1)
+
+slurid_temp_pred_mat <- bind_rows(slurid_temp_abs, slurid_temp_max)
+
+
+slurid_temp_predict <- predict_MRF(slurid_temp_pred_mat, herb_mod) %>% 
+  `colnames<-`(herbivores) %>% 
+  as_data_frame() %>% 
+  mutate(temp = slurid_temp_pred_mat$temp)
+
+# Plot
+abs_pred <- slurid_temp_predict %>% 
+  dplyr::slice_head(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_abs")
+pres_pred <- slurid_temp_predict %>% 
+  dplyr::slice_tail(prop = 0.5) %>% 
+  pivot_longer(all_of(herbivores),
+               names_to = "species",
+               values_to = "pred_pres")
+
+slurid_predictions <- abs_pred %>% left_join(pres_pred) %>% 
+  pivot_longer(cols = pred_abs:pred_pres,
+               names_to = "model",
+               values_to = "prediction")
+
+# Plot the predictions:
+plot_predictions(slurid_predictions, herbivores[[2]])
+# ggsave(filename = "figures/predictions/Sluridus_Srivulatus_temp_nonspat.png", plot = last_plot(), dpi = 300, device = "png")
 
