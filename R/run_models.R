16b761d0c6fe98a05da8b5634c777ac0ee36e9ef
Shira Salingre <shira.salin@gmail.com>
2020-11-24 10:07:34 +0200

    model corrected from family = 'gaussian' to 'poisson'

--- R/run_models.R
+++ R/run_models.R
@@ -6,18 +6,15 @@
 
 # Groupers ----------------------------------------------------------------
 
-grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 4, family = "gaussian")
-grps_mod_nocov <- MRFcov(data = grps_mat[1:length(groupers)], n_nodes = length(groupers), family = "gaussian")
+grps_mod <- MRFcov(data = grps_mat, n_nodes = length(groupers), n_covariates = 4, family = "poisson")
 
-# Seabream ----------------------------------------------------------------
+# Seabreams ---------------------------------------------------------------
 
-dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 4, family = "gaussian")
-dip_mod_nocov <- MRFcov(data = dip_mat[1:length(diplodus)], n_nodes = length(diplodus), family = "gaussian")
+dip_mod <- MRFcov(data = dip_mat, n_nodes = length(diplodus), n_covariates = 4, family = "poisson")
 
 # Herbivores --------------------------------------------------------------
 
-herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 4, family = "gaussian")
-herb_mod_nocov <- MRFcov(data = herb_mat[1:length(herbivores)], n_nodes = length(herbivores), family = "gaussian")
+herb_mod <- MRFcov(data = herb_mat, n_nodes = length(herbivores), n_covariates = 4, family = "poisson")
 
 # Take a look at the output of the models:
 grps_mod
@@ -27,20 +24,25 @@ grps_mod$direct_coefs # association parameters of species and covariates
 # grps_mod$direct_coefs[1,] %>% View
 
 
-# Figure 1. Co-occurrence of species with and without covariates ----------
-# Compare between co-occurrence with and without covariates:
+
+# Species co-occurrence with and without covariates -----------------------
+
 # GROUPERS
 grpsHM_cov <- plotMRF_hm(grps_mod, main = "with covariates")
+grps_mod_nocov <- MRFcov(data = grps_mat[1:length(groupers)], n_nodes = length(groupers), family = "poisson")
 grpsHM_nocov <- plotMRF_hm(grps_mod_nocov, main = "without covariates")
 # gridExtra::grid.arrange(grpsHM_cov, grpsHM_nocov, nrow = 1, top = "Groupers co-occurrence")
 
 # SEABREAM
 dipHM_cov <- plotMRF_hm(dip_mod, main = "with covariates")
+dip_mod_nocov <- MRFcov(data = dip_mat[1:length(diplodus)], n_nodes = length(diplodus), family = "poisson")
 dipHM_nocov <- plotMRF_hm(dip_mod_nocov, main = "without covariates")
+
 # gridExtra::grid.arrange(dipHM_cov, dipHM_nocov, nrow = 1, top = "Seabream co-occurrence")
 
 # HERBIVORES
 herbHM_cov <- plotMRF_hm(herb_mod, main = "with covariates")
+herb_mod_nocov <- MRFcov(data = herb_mat[1:length(herbivores)], n_nodes = length(herbivores), family = "poisson")
 herbHM_nocov <- plotMRF_hm(herb_mod_nocov, main = "without covariates")
 # gridExtra::grid.arrange(herbHM_cov, herbHM_nocov, nrow = 1, top = "Herbivores co-occurrence")
 
