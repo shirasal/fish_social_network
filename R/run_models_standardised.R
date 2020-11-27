1c9d75491e5e6c6e7de5db56c71887fe558c6161
Shira Salingre <shira.salin@gmail.com>
2020-11-27 08:35:38 +0200

    herbivore models and predictions created

--- R/run_models_standardised.R
+++ R/run_models_standardised.R
@@ -37,5 +37,26 @@ lapply(dip_mod_std$key_coefs, function(x) x %>%
          filter(str_detect(string = Variable, pattern = "_")))
 
 vis_temp_pred_pair(species_i = "Diplodus.annularis", species_j = "Diplodus.vulgaris", 
-                   spp_mat = dip_std, spp_mod = dip_mod_std, guild = diplodus, n_spp = 5) %>% 
-  ggsave(filename = "figures/predictions/D_annularis-D_vulgaris--TEMP_SCALED.png", device = "png")
+                  spp_mat = dip_std, spp_mod = dip_mod_std, guild = diplodus, n_spp = 5)# %>% 
+  # ggsave(filename = "figures/predictions/D_annularis-D_vulgaris--TEMP_SCALED.png", device = "png")
+
+herb_std <- med_data %>% 
+  group_by_at(.vars = cols) %>% # group for summarise
+  summarise(n = sum(abund_std)) %>% # sum sp.n for each grouped variable
+  spread(species, n, fill = 0) %>% # convert to species matrix
+  ungroup() %>% 
+  na.omit() %>% # remove NAs; make sure this part it minimised in the raw data
+  mutate(loc = paste(site, trans)) %>% # Create a variable of the location, which should be unique
+  group_by(loc) %>% 
+  column_to_rownames("loc") %>% # create row names by location
+  select(all_of(herbivores), mpa, temp, depth, prod)
+
+herb_mod_std <- MRFcov(herb_std, n_nodes = 4, family = "gaussian")
+lapply(herb_mod_std$key_coefs, function(x) x %>% 
+         filter(Rel_importance > 0.1) %>% 
+         filter(str_detect(string = Variable, pattern = "_")))
+
+vis_mpa_pred_pair(species_i = "Sparisoma.cretense", species_j = "Siganus.luridus", 
+                  spp_mat = herb_std, spp_mod = herb_mod_std, guild = herbivores, n_spp = 4)
+
+vis_temp_pred_pair("Sparisoma.cretense", "Sarpa.salpa", herb_std, herb_mod_std, herbivores, 4)
