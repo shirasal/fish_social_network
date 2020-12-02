5a25a4fefb930c9381f4ef8cc02fa705814d18ad
Shira Salingre <shira.salin@gmail.com>
2020-12-02 21:45:53 +0200

    prediction visualisations functions added

--- R/new_analyses.R
+++ R/new_analyses.R
@@ -86,6 +86,134 @@ plot_relimp <- function(rel_imp_df, col, guild_name){
           strip.text.x = element_text(size = 12, face = "bold"))
 }
 
+### MPA predictions visualisation
+vis_mpa_pred_pair <- function(species_i, species_j, spp_mat, spp_mod, guild, n_spp){
+  # Create a vector of all other species except species_j
+  all_other_species <- guild[-which(guild == species_j)]
+  
+  # Scenario 1: species_j is absent, other species are at their mean abundance
+  j_abs <- spp_mat %>% 
+    mutate(temp = median(temp),
+           depth = median(depth),
+           prod = median(prod),
+           across(.cols = all_of(species_j), .fns = function(x) 0),
+           across(all_of(all_other_species), .fns = mean)) %>% 
+    group_by(mpa) %>% 
+    sample_n(1)
+  # Scenario 2: species_j is at its max abundance, other species are at their mean abundance
+  j_max <- spp_mat %>% 
+    mutate(temp = median(temp),
+           depth = median(depth),
+           prod = median(prod),
+           across(.cols = all_of(species_j), .fns = max),
+           across(all_of(all_other_species), .fns = mean)) %>% 
+    group_by(mpa) %>% 
+    sample_n(1)
+  
+  # Create predictions
+  ## For when species j is absent
+  predict_abs <- predict_MRF(j_abs, spp_mod) %>% 
+    `colnames<-`(guild) %>% 
+    as_data_frame() %>% 
+    mutate(mpa = j_abs$mpa)
+  ## For when species j is absent
+  predict_max <- predict_MRF(j_max, spp_mod) %>% 
+    `colnames<-`(guild) %>% 
+    as_data_frame() %>% 
+    mutate(mpa = j_max$mpa)
+  
+  # Put the two scenarios together
+  mpa_predict <- bind_rows(predict_abs, predict_max, .id = "scenario") %>% 
+    mutate(scenario = case_when(scenario == 1 ~ "absent",
+                                scenario == 2 ~ "present"))
+  
+  # Visualise the predictions
+  ## Create a dataframe with all the predictions, sorted by scenario
+  predictions_mpa <- mpa_predict %>% 
+    pivot_longer(cols = all_of(2:(1+n_spp)),
+                 names_to = "species",
+                 values_to = "prediction", 
+                 names_repair = "minimal")
+  
+  ## Plot the predictions:
+  predictions_mpa %>%
+    filter(species == species_i) %>% 
+    ggplot() +
+    aes(x = mpa, y = prediction, fill = scenario) +
+    stat_summary(geom = "bar", fun = "mean", position = "dodge") +
+    # stat_summary(geom = "errorbar", fun.data = "mean_se", position = position_dodge(width = 0.8), width = 0.2) +
+    xlab("MPA") + ylab("Prediction") +
+    labs(title = "Observation predictions",
+         subtitle = stringr::str_replace(species_i, "\\.", "\\ "),
+         fill = stringr::str_replace(species_j, "\\.", "\\ ")) +
+    scale_fill_manual(labels = c('Absent','Present'), values = c("#031D44", "#FF99C9")) +
+    theme(legend.title = element_text(face = "italic"), plot.subtitle = element_text(face = "italic"))
+}
+
+### Temperature predictions visualisation
+vis_temp_pred_pair <- function(species_i, species_j, spp_mat, spp_mod, guild, n_spp){
+  # Create a vector of all other species except species_j
+  all_other_species <- guild[-which(guild == species_j)]
+  
+  # Scenario 1: species_j is absent, other species are at their mean abundance
+  j_abs <- spp_mat %>% 
+    mutate(depth = median(depth),
+           prod = median(prod),
+           mpa = TRUE,
+           across(.cols = all_of(species_j), .fns = function(x) 0),
+           across(all_of(all_other_species), .fns = mean)) %>% 
+    group_by(temp = round(temp, digits = 1)) %>% 
+    sample_n(1)
+  # Scenario 2: species_j is at its max abundance, other species are at their mean abundance
+  j_max <- spp_mat %>% 
+    mutate(depth = median(depth),
+           prod = median(prod),
+           mpa = TRUE,
+           across(.cols = all_of(species_j), .fns = max),
+           across(all_of(all_other_species), .fns = mean)) %>% 
+    group_by(temp = round(temp, digits = 1)) %>% 
+    sample_n(1)
+  
+  # Create predictions
+  ## For when species j is absent
+  predict_abs <- predict_MRF(j_abs, spp_mod) %>% 
+    `colnames<-`(guild) %>% 
+    as_data_frame() %>% 
+    mutate(temp = j_abs$temp)
+  ## For when species j is absent
+  predict_max <- predict_MRF(j_max, spp_mod) %>% 
+    `colnames<-`(guild) %>% 
+    as_data_frame() %>% 
+    mutate(temp = j_max$temp)
+  
+  # Put the two scenarios together
+  temp_predict <- bind_rows(predict_abs, predict_max, .id = "scenario") %>% 
+    mutate(scenario = case_when(scenario == 1 ~ "absent",
+                                scenario == 2 ~ "present"))
+  
+  # Visualise the predictions
+  ## Create a dataframe with all the predictions, sorted by scenario
+  predictions_temp <- temp_predict %>% 
+    pivot_longer(cols = all_of(2:(1+n_spp)),
+                 names_to = "species",
+                 values_to = "prediction", 
+                 names_repair = "minimal")
+  
+  ## Plot the predictions:
+  predictions_temp %>%
+    filter(species == species_i) %>% 
+    ggplot() +
+    aes(x = temp, y = prediction, col = scenario) +
+    geom_smooth(method = "lm", formula = y ~ x, cex = 3, alpha = 0.1) +
+    xlab("Temperature (scaled)") + ylab("Prediction") +
+    labs(title = "Observation predictions",
+         subtitle = stringr::str_replace(species_i, "\\.", "\\ "),
+         col = stringr::str_replace(species_j, "\\.", "\\ ")) +
+    scale_color_manual(labels = c('Absent','Present'), values = c("#031D44", "#FF99C9")) +
+    theme(legend.title = element_text(face = "italic"), plot.subtitle = element_text(face = "italic"))
+}
+
+
 # Data --------------------------------------------------------------------
 
 ### Variables (fitting with med_clean)
@@ -439,6 +567,7 @@ dip_rank <- dip_mat %>%
 herb_rank <- herb_mat %>% 
   mutate(across(1:4, .fns = rank))
 
+# Check the rankings, because something here is odd
 t <- grps_rank %>% 
   pivot_longer(cols = all_of(groupers), names_to = "species", values_to = "rank")
 median(t$rank)
@@ -499,7 +628,11 @@ egg::ggarrange(p_relimp_grps_rank, p_relimp_dip_rank, p_relimp_herb_rank) %>%
   ggsave(filename = "rel_imp_rank_nonspat.png", device = "png", path = "figures/rel_imp/", 
          dpi = 150, height = 10, width = 10, units = "in")
 
- 
+
+
+# Visualisations ----------------------------------------------------------
+
+
 
 
 
