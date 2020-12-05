d8a9dcbb68a7bb1d5e3959be033cb6cddb434004
Shira Salingre <shira.salin@gmail.com>
2020-12-05 15:01:20 +0200

    added overall networks

--- R/new_analyses.R
+++ R/new_analyses.R
@@ -1038,3 +1038,32 @@ grps_pois_spat_relimp
 
 dip_pois_relimp
 dip_pois_spat_relimp
+
+
+# Overall networks --------------------------------------------------------
+
+library(ggraph)
+
+plot_graph <- function(guild_mod, plot_title){
+  net_cols <- c(neg = '#FF3333', pos = '#3399CC')
+  net <- igraph::graph.adjacency(guild_mod$graph, weighted = T, mode = "undirected")
+  weights <- igraph::E(net)$weight
+  deg <- igraph::degree(net, mode = "all")
+  ggraph(net, layout = "circle") + 
+    geom_edge_link(aes(width = weights, color = weights < 0), lineend = "round", linejoin = "round") +
+    scale_edge_width(range = c(0, 3)) +
+    scale_edge_color_manual(values = c(net_cols[["pos"]], net_cols[["neg"]])) +
+    geom_node_point(aes(size = deg), col = "grey", alpha = 0.5) +
+    geom_node_text(aes(label = name), repel = TRUE, check_overlap = TRUE, 
+                   point.padding = unit(0.2, "lines")) +
+    theme(legend.position = "none",
+          aspect.ratio = 1,
+          panel.background = element_blank()) +
+    ggtitle(plot_title)
+}
+
+plot_graph(grps_pois, plot_title = "Groupers network")
+ggsave("figures/networks/groupers_network.png", device = "png", dpi = 300)
+
+plot_graph(dip_pois, "Seabreams network")
+ggsave("figures/networks/diplodus_network.png", device = "png", dpi = 300)
