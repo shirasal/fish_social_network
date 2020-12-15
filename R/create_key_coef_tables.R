5950ea4a85f620ac58f5dd5df48a5f9786335e8f
Shira Salingre <shira.salin@gmail.com>
2020-12-15 15:45:51 +0200

    correct species names in key coef tables

--- R/create_key_coef_tables.R
+++ R/create_key_coef_tables.R
@@ -1,6 +1,7 @@
 bind_rows(grps_pois$key_coefs, .id = "name") %>% arrange(name, Variable) %>% 
   separate(col = Variable, into = c("Covariate", "Interaction"), sep = "_") %>% 
   mutate(name = str_replace(name, "\\.", "\\ "),
+         Covariate = str_replace(Covariate, "\\.", "\\ "),
          Interaction = str_replace(Interaction, "\\.", "\\ ")) %>% 
   rename(Species = name, Coefficient = Raw_coef) %>% select(-Standardised_coef) %>% 
   write_csv("data/grps_key_coefs.csv")
@@ -8,11 +9,13 @@ bind_rows(grps_pois$key_coefs, .id = "name") %>% arrange(name, Variable) %>%
 bind_rows(dip_pois$key_coefs, .id = "name") %>% arrange(name, Variable) %>%
   separate(col = Variable, into = c("Covariate", "Interaction"), sep = "_") %>% 
   mutate(name = str_replace(name, "\\.", "\\ "),
+         Covariate = str_replace(Covariate, "\\.", "\\ "),
          Interaction = str_replace(Interaction, "\\.", "\\ ")) %>% 
   rename(Species = name, Coefficient = Raw_coef) %>% select(-Standardised_coef) %>% write_csv("data/dip_key_coefs.csv")
 
 bind_rows(herb_pois$key_coefs, .id = "name") %>% arrange(name, Variable) %>%
   separate(col = Variable, into = c("Covariate", "Interaction"), sep = "_") %>% 
   mutate(name = str_replace(name, "\\.", "\\ "),
+         Covariate = str_replace(Covariate, "\\.", "\\ "),
          Interaction = str_replace(Interaction, "\\.", "\\ ")) %>% 
   rename(Species = name, Coefficient = Raw_coef) %>% select(-Standardised_coef) %>% write_csv("data/herb_key_coefs.csv")
