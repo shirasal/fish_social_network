d32c65b7af8b02d21b0bb2962944ad6c2af01b3a
Shira Salingre <shira.salin@gmail.com>
2020-12-19 19:16:50 +0200

    checked CRF/MRF comparison with groupers and diplodus

--- R/comparing_crf_mrf_spatial.R
+++ R/comparing_crf_mrf_spatial.R
@@ -77,35 +77,30 @@ t.test(spat.mse, nonspat.mse, paired = TRUE, alternative = "greater")
 
 # Not sure this is valid..
 
-# CRF-MRF
+
+
+# Compare CRF and MRF -----------------------------------------------------
+# MRFs = no covariates
+
+comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = grps_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 4, plot = FALSE)
+
+MRFcov::cv_MRF_diag_rep(data = grps_mat, family = "poisson", n_nodes = 4, compare_null = TRUE, plot = TRUE)
+
+MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
+CRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], 0.5)
+
+if (which.min(c(MRF.mse, CRF.mse)) == 1) {
+  best.mod <- "MRF"
+} else {
+  best.mod <- "CRF"
+}
+paste("Groupers best model:", best.mod)
+
+
+
 comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = dip_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 4, plot = FALSE)
 
-MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
-CRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], 0.5)
-
-if (which.min(c(MRF.mse, CRF.mse)) == 1) {
-  best.mod <- "MRF"
-} else {
-  best.mod <- "CRF"
-}
-best.mod
-
-
-# Herbivores --------------------------------------------------------------
-
-comparison_spatial <- MRFcov::cv_MRF_diag_rep_spatial(data = herb_mat, coords = herb_coords, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
-
-Spatial.mse <- quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Spatial MRF")], 0.5)
-Nonspatial.mse <- quantile(comparison_spatial$MSE[which(comparison_spatial$model == "Non-spatial MRF")], 0.5)
-
-if (which.min(c(Spatial.mse, Nonspatial.mse)) == 1) {
-  best.mod <- "Spatial"
-} else {
-  best.mod <- "Nonspatial"
-}
-best.mod
-
-comparison_crf_mrf <- MRFcov::cv_MRF_diag_rep(data = herb_mat, n_covariates = 4, compare_null = TRUE, family = "poisson", n_nodes = 5, plot = FALSE)
+MRFcov::cv_MRF_diag_rep(data = dip_mat, family = "poisson", n_nodes = 4, compare_null = TRUE, plot = TRUE)
 
 MRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "MRF (no covariates)")], 0.5)
 CRF.mse <- quantile(comparison_crf_mrf$MSE[which(comparison_crf_mrf$model == "CRF")], 0.5)
@@ -115,7 +110,5 @@ if (which.min(c(MRF.mse, CRF.mse)) == 1) {
 } else {
   best.mod <- "CRF"
 }
-best.mod
-
-
+paste("Seabreams best model:", best.mod)
 
