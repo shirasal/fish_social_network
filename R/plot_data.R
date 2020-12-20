283a9edc9a5745e87044d3d33523446732972ae4
Shira Salingre <shira.salin@gmail.com>
2020-12-20 08:11:33 +0200

    created histograms for all species

--- R/plot_data.R
+++ R/plot_data.R
@@ -31,7 +31,7 @@ species_temp <- lapply(as.list(c(groupers, diplodus, herbivores)), function(sp) 
 
 ggpubr::ggarrange(plotlist = species_temp, ncol = 5, nrow = 3) %>% 
   ggpubr::annotate_figure(bottom = ggpubr::text_grob("Temperature (C)"),
-                left = ggpubr::text_grob("Abundance", rot = 90))
+                          left = ggpubr::text_grob("Abundance", rot = 90))
 ggsave("species_dist_temp.png", device = "png", path = "figures", width = 15, height = 10, units = "in")
 
 
@@ -52,7 +52,9 @@ med_raw %>%
   facet_wrap(~species) + 
   xlab("MPA") + ylab("Abundance (log)") +
   scale_fill_manual("Guild", 
-                     values = c("Groupers" = "#eccbae", "Seabreams" = "#d29a4c" , "Herbivores" = "#145d82")) +
+                    values = c("Groupers" = guild_colours$grps,
+                               "Seabreams" = guild_colours$dip ,
+                               "Herbivores" = guild_colours$herb)) +
   theme(strip.text = element_text(face = "italic"))
 
 species_mpa <- lapply(as.list(c(groupers, diplodus, herbivores)), function(sp) {
@@ -72,3 +74,22 @@ ggpubr::ggarrange(plotlist = species_mpa, ncol = 5, nrow = 3) %>%
   ggpubr::annotate_figure(bottom = ggpubr::text_grob("MPA"),
                           left = ggpubr::text_grob("Abundance", rot = 90))
 ggsave("species_dist_mpa.png", device = "png", path = "figures", width = 15, height = 10, units = "in")
+
+
+med_raw %>% 
+  filter(species == all_of(c(groupers, diplodus, herbivores))) %>% 
+  filter(data.origin != "azz_asi") %>% 
+  mutate(guild = case_when(species %in% groupers ~ "Groupers",
+                           species %in% diplodus ~ "Seabreams",
+                           species %in% herbivores ~ "Herbivores")) %>%
+  arrange(guild, species) %>%
+  ggplot() + 
+  geom_histogram(aes(sp.n, fill = guild), binwidth = .25) +
+  scale_x_log10() + 
+  facet_wrap(~species, scales = "free_y") + 
+  xlab("Abundance") + ylab("Frequency") +
+  scale_fill_manual("Guild", 
+                     values = c("Groupers" = guild_colours$grps,
+                                "Seabreams" = guild_colours$dip ,
+                                "Herbivores" = guild_colours$herb)) +
+  theme(strip.text = element_text(face = "italic"))
