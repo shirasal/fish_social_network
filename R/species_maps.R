92084a3c51b64961b15eb46cd7b4d1b9a12db181
Shira Salingre <shira.salin@gmail.com>
2020-12-25 22:12:52 +0200

    improved species maps

--- R/species_maps.R
+++ R/species_maps.R
@@ -2,7 +2,7 @@
 # load("data/data_and_objects.RData")
 
 # Base map of the Mediterranean Sea
-med_seas <- sf::st_read("C:/Users/shira/Documents/MSc/medata/Med_World_Seas_IHO_v3_MarineRegions/Medit_seas_IHO.shp")
+med_seas <- sf::st_read("C:/Users/shira/Documents/MSc/GIS/Mediterranean Sea/Mediterranean_Sea_Area.shp")
 species_locations <- guilds_data %>% select(lon, lat, species, abundance, guild) %>% arrange(guild)
 spp_list <- NULL
 for (i in unique(species_locations$species)){
@@ -14,11 +14,14 @@ spp_list[[5]] # Check
 spp_maps <- list()
 for (j in 1:length(spp_list)) {
   spp_maps[[j]] <- ggplot(data = spp_list[[j]]) +
-    geom_sf(data = med_seas, colour = "black", fill = "#00E5E5", alpha = 0.3) +
-    geom_point(aes(x = lon, y = lat), colour = "#F7347A", size = 5, alpha = 0.6) +
+    geom_sf(data = med_seas, colour = "black", fill = "#b0ddf4") +
+    geom_point(aes(x = lon, y = lat), colour = "#f7c11c", size = 5, alpha = 0.6) +
     ggtitle(label = str_replace(spp_list[[j]]$species, "\\.", "\\ ")) +
     xlab("") + ylab("") + 
-    theme(title = element_text(face = "italic"))
+    theme_bw() +
+    theme(title = element_text(face = "italic"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
   
   # ggsave(filename = paste0(spp_list[[j]]$species, "_raw_obs.png"), path = "figures/species_maps/", plot = last_plot(),
   #        width = 12, height = 8, units = "in", dpi = 300, device = "png")
